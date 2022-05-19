9b32c4fd8e908582cba9d523af9964a350103433
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-05-19 11:23:17 -0400

    more updates. bootstrapped react to django

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('leads.urls'))
 ]
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
