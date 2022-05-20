b7fe2d21ebf2f392681c50d9cb731e1749f02844
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-05-20 12:04:39 -0400

    finished bootstrapping backend to the frontend. All data is now presented in the UI: the ID, Name, Email, Message, and a soon functional delete button which will allow the user to delete users. Soon I will allow the front end to communicate with the back end and allow the users input their own information rather than in Postman. Table also implemented. No bugs.

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getLeads } from '../../actions/leads'; 
+import { getLeads } from '../../actions/leads';
 
 
 export class Leads extends Component {
-static propTypes = {
-  leads: PropTypes.array.isRequired
-}
+  static propTypes = {
+    leads: PropTypes.array.isRequired
+  }
 
   componentDidMount() {
     this.props.getLeads();
@@ -15,13 +15,38 @@ static propTypes = {
 
   render() {
     return (
-      <div>Leads List</div>
-    )
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-strped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+           {this.props.leads.map(lead => (
+               <tr key = {lead.id}>
+                 <td>{lead.id}</td>
+                 <td>{lead.name}</td>
+                 <td>{lead.email}</td>
+                 <td>{lead.message}</td>
+                 <td><button className = "btn btn-danger btn-sm">Delete</button></td>
+               </tr>
+             ))
+           }
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   leads: state.leads.leads
 });
-  
-export default connect(mapStateToProps, {getLeads})(Leads);
+
+export default connect(mapStateToProps, { getLeads })(Leads);
