f153a8f688a66ab6d5598a864a1a3856fe12bf0c
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-06 13:02:35 -0400

    typo

--- leadmanager/frontend/src/features/profiles/Profiles.js
+++ leadmanager/frontend/src/features/profiles/Profiles.js
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
 
-const LeadList = () => {
+const Profiles = () => {
     const [newLead, setNewLead] = useState('')
 
 
