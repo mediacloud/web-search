f24d9e7233b66c5b4027d06e6379859d146cf158
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-07 10:29:11 -0400

    getting the information, it's not clean or organized but more importantly the information is being displayed. The html/css used for the previous leads will be implemented

--- leadmanager/frontend/src/features/profiles/Profiles.js
+++ leadmanager/frontend/src/features/profiles/Profiles.js
@@ -7,7 +7,7 @@ import {
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
-import React, { useState } from "react"
+import React, { Fragment, useState } from "react"
 
 
 const Profiles = () => {
@@ -31,14 +31,15 @@ const Profiles = () => {
         val++;
         e.preventDefault();
         // addTodo
-        setNewLead({ "id": val, "first_name": newLead, "last_name": newLead, "email": newLead, "message": newLead, completed: false })
+        console.log(newLead);
+        //addLead({ "id": val, "first_name": newLead, "last_name": newLead, "email": newLead, "message": newLead, completed: false })
         setNewLead('')
     }
 
 
     const newItemSection =
         <form onSubmit={handleSubmit}>
-           
+
             <div className="new-lead">
                 <input
                     type="text"
@@ -47,10 +48,11 @@ const Profiles = () => {
                     onChange={(e) => setNewLead(e.target.value)}
                     placeholder="Enter new lead"
                 />
+
+                <button className="submit">
+                    <FontAwesomeIcon icon={faUpload} />
+                </button>
             </div>
-            <button className="submit">
-                <FontAwesomeIcon icon={faUpload} />
-            </button>
         </form>
 
 
@@ -60,25 +62,53 @@ const Profiles = () => {
     if (isLoading) {
         content = <p>Loading...</p>
     } else if (isSuccess) {
-        content = leads.map(lead => {
-            return (
-                <article key={lead.id}>
-                    <div className="lead">
-                        <input
-                            type="checkbox"
-                            checked={lead.completed}
-                            id={lead.id}
-                            onChange={() => updateLead({ ...lead, completed: !lead.completed })}
-                        />
-                        <label htmlFor={lead.id}>{lead.title}</label>
-                    </div>
-                    <button className="trash" onClick={() => ({ id: lead.id })}>
-                        <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                </article>
-            )
-        })
-    } else if (isError) {
+        content =
+            <Fragment>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Email</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbidy>
+                        {leads.map(lead => { //JSON.stringify(leads)
+                            // returning an article which will have an article and each artciel is going to have a key
+                            // that matches the lead.id 
+                            // lots of artciles will be run 
+                            return (
+                                <article key={lead.id}>
+                                    <div className="lead">
+                                        <input
+                                            type="checkbox"
+                                            checked={lead.completed}
+                                            id={lead.id}
+                                            onChange={() => updateLead({ ...lead, completed: !lead.completed })}
+                                        />
+                                        <label htmlFor={lead.id}>{lead.title}</label>
+                                        <td>{lead.id}</td>
+                                        <td>{lead.first_name}</td>
+                                        <td>{lead.last_name}</td>
+                                        <td>{lead.email}</td>
+                                        <td>{lead.message}</td>
+
+
+                                    </div>
+                                    <button className="trash" onClick={() => ({ id: lead.id })}>
+                                        <FontAwesomeIcon icon={faTrash} />
+                                    </button>
+                                </article>
+                            )
+                        })}
+                    </tbidy>
+                </table>
+            </Fragment>
+
+    }
+    else if (isError) {
         content = <p>{error}</p>
     }
     return (
