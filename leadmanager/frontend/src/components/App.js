03122dfdbfe32f3d70fd1328c92219a065b6ccea
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-07 16:23:26 -0400

    implementing react router to allow changing the brower url and ui in sync

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,6 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
 
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
+
+
 import Header from './layout/Header';
 import Profiles from '../features/profiles/Profiles';
 
@@ -13,19 +23,47 @@ import { apiSlice } from '../features/api/apiSlice';
 class App extends Component {
     render() {
         return (
-            <ApiProvider api= {apiSlice}>
+            <ApiProvider api={apiSlice}>
                 <Fragment>
                     <Header />
                     <Profiles />
-                </Fragment>
-            </ApiProvider>
 
+                </Fragment>
+
+                <Link to="/header">Header</Link>
+                <Link to="/profiles">Profiles</Link >
+
+
+            </ApiProvider >
         );
+
+
+
+        /*
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="admin" element={<AdminHome />}>
+                        <Route path="users" element={<UserList />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+        */
 
     }
 }
 
-createRoot(document.getElementById('app')).render(<App tab="home" />);
+createRoot(document.getElementById('app')).
+    render(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="header" element={<Header />} />
+                    <Route path="users" element={<Profiles />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>);
 
 
 
