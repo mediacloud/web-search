d133477cce1bf91e1f302a193950e1542dba9933
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-10 14:33:10 -0400

    print the data to show that I can access it

--- leadmanager/frontend/src/routes/invoices.js
+++ leadmanager/frontend/src/routes/invoices.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-export default function Invoices() {
+import { Link } from "react-router-dom";
+import { getInvoices } from "../data";
 
+export default function Invoices() {
+    let invoices = getInvoices(); 
+
+    for (const element of invoices) {
+        console.log(element)
+    }
+
+
+    
     const mainStyle = {
         padding: "1rem 0"
     }
 
     return (
+        
         <main style={mainStyle}>
             <h2>Invoices</h2>
         </main>
