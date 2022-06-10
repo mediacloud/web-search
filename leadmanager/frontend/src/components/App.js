341ae221dc080509b4754064240be3aa8035e636
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-10 15:17:16 -0400

    fill in empty space

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -46,6 +46,14 @@ createRoot(document.getElementById('app')).
                 <Route path='/' element={<App />}>
                     <Route path='expenses' element={<Expenses />} />
                     <Route path="invoices" element={<Invoices />}>
+                        <Route
+                            index
+                            element={
+                                <main style={{ padding: "1rem" }}>
+                                    <p>Select an invoice</p>
+                                </main>
+                            }
+                        />
                         <Route path=":invoiceId" element={<Invoice />} />
                     </Route>
                     <Route
