39a591f62d5d22a328ecbe4a42b4f2ffa22c021c
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-10 13:07:12 -0400

    creating invoices and routes

---
+++ leadmanager/frontend/src/routes/expenses.js
@@ -0,0 +1,12 @@
+export default function Expenses() {
+
+    const mainStyle = {
+        padding: "1rem 0"
+    }
+
+    return (
+        <main style={mainStyle}>
+            <h2>Expenses</h2>
+        </main>
+    );
+}
---
+++ leadmanager/frontend/src/routes/invoices.js
@@ -0,0 +1,12 @@
+export default function Invoices() {
+
+    const mainStyle = {
+        padding: "1rem 0"
+    }
+
+    return (
+        <main style={mainStyle}>
+            <h2>Invoices</h2>
+        </main>
+    );
+}
