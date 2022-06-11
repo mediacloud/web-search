0a295ce5309247aa61288da06c8adcc46a05a504
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-11 14:46:27 -0400

    added headbar to homepage

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -14,59 +14,31 @@ import Expenses from '../routes/expenses';
 import Invoices from '../routes/invoices';
 import Invoice from '../routes/Invoice';
 
+import Homepage from './layout/Homepage';
 
 import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import { apiSlice } from '../features/api/apiSlice';
 
 
 
+
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Bookkeeper</h1>
-                <nav style={{
-                    borderBottom: "solid 1px",
-                    paddingBottom: "1rem",
-                }}>
-                    <Link to="/invoices">Invoices</Link>|{" "}
-                    <Link to="/expenses">Expenses</Link>
-                </nav>
-                <Outlet />
-            </div>
+            <ApiProvider api={apiSlice}>
+                <Homepage />
+            </ApiProvider >
         );
-
-
 
     }
 }
+        
 
 createRoot(document.getElementById('app')).
     render(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<App />}>
-                    <Route path='expenses' element={<Expenses />} />
-                    <Route path="invoices" element={<Invoices />}>
-                        <Route
-                            index
-                            element={
-                                <main style={{ padding: "1rem" }}>
-                                    <p>Select an invoice</p>
-                                </main>
-                            }
-                        />
-                        <Route path=":invoiceId" element={<Invoice />} />
-                    </Route>
-                    <Route
-                        path="*"
-                        element={
-                            <main style={{ padding: "1rem" }}>
-                                <p>There's nothing here!</p>
-                            </main>
-                        }
-                    />
-                </Route>
+                <Route path='/' element={<App />} />
             </Routes>
         </BrowserRouter >
     );
