9328f6332a3bf234ff37d9d30e2f1aa1d32b0fe5
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-13 11:34:44 -0400

    created a sotre

--- leadmanager/frontend/src/store.js
+++ leadmanager/frontend/src/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit/query/react';
-import { setupListeners } from '@reduxjs/toolkit/query';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { leadsApi } from './features/api/leads';
 
 export const store = configureStore({
@@ -15,4 +15,4 @@ export const store = configureStore({
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
-//setupListeners(store.dispatch)
+setupListeners(store.dispatch)
