f219ce9c2b20b4c4a3059a8450bb9143b8f0b63f
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-16 12:53:43 -0400

    removed user list

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -6,7 +6,6 @@ import { Outlet, Link } from 'react-router-dom';
 import { ApiProvider } from '@reduxjs/toolkit/query/react';
 
 import Header from './layout/Header';
-import UserList from '../features/profiles/UserList';
 import Homepage from './layout/Homepage';
 import { getStore } from '../store';
 
@@ -23,7 +22,6 @@ export const renderApp = () => {
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route path="header" element={<Header />} />
-                    <Route path="users" element={<UserList />} />
                 </Route>
             </Routes>
         </BrowserRouter>
