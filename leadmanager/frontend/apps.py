a64b1ebb03c69e14cfad10dc374e88df5cd93bca
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-16 18:01:45 -0400

    added comments

--- leadmanager/frontend/apps.py
+++ leadmanager/frontend/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# creating 'frontend' to be imported into leadmanager INSTALLED_APPS
 class FrontendConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "frontend"
