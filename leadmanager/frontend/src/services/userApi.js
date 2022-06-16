0f2f250b9c9b7826e2d9541ad58a9ba7ed0d2b9b
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-16 12:55:25 -0400

    cleaned code for readability

--- leadmanager/frontend/src/services/userApi.js
+++ leadmanager/frontend/src/services/userApi.js
@@ -6,23 +6,27 @@ export const userApiSlice = createApi({
   reducerPath: 'users',
   initialState,
   baseQuery: fetchBaseQuery({ baseUrl: '/api/users' }),
+
   prepareHeaders: (headers, { getState }) => {
     // Django requires this for security (cross-site forgery protection) once logged in
     headers.set('X-CSRFToken', window.CSRF_TOKEN);
     return headers;
   },
   tagTypes: ['user'],
+
   endpoints: (builder) => ({
     current: builder.query({
       query: () => 'current',
       transformResponse: res => ({ ...res, isLoggedIn: res.is_authenticated }),
       providesTags: ['user']
     }),
+
     logout: builder.query({
       query: () => 'logout',
-      transformResponse: res => { isLoggedIn: false },
+      transformResponse: res => { isLoggedIn = false },
       invalidatesTags: ['user']
     }),
+    
   })
 })
 
