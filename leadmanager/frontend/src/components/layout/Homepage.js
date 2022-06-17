61b383adf3c34d357015fb764c9a368f77a531a4
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-16 23:23:19 -0400

    added ui, buttons (login,logout)

--- leadmanager/frontend/src/components/layout/Homepage.js
+++ leadmanager/frontend/src/components/layout/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Header from './Header';
+import Login from './Login'
 import { selectCurrentUser } from '../../services/userApi';
 
 
@@ -27,16 +28,17 @@ const Homepage = () => {
   console.log(currentUser);
 
   return (
-      <Fragment>
+      <>
           <Header />
-          <div style={divStyle} >
+          {/* <div style={divStyle} >
               <h1 style={h1Style}>Welcome to Media Cloud</h1>
               {currentUser.isLoggedIn && (<h3>Welcome back {currentUser.username}</h3>)}
               {!currentUser.isLoggedIn && (<h3>You're not logged in</h3>)}
               <Link to="/header" style={linkStyle}>Header</Link>
               <Link to="/profiles" style={linkStyle}>Profiles</Link >
-          </div>
-      </Fragment >
+          </div> */}
+          <Login />
+      </>
 
   );
 }
