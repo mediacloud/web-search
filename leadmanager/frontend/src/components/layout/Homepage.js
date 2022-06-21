a26e178ce44e5039e58c5916ff10c11b82740986
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-21 13:52:50 -0400

    moved theme and container to homepage?

--- leadmanager/frontend/src/components/layout/Homepage.js
+++ leadmanager/frontend/src/components/layout/Homepage.js
@@ -5,53 +5,23 @@ import Header from './Header';
 import Login from './Login'
 import { selectCurrentUser } from '../../services/userApi';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
 
+const theme = createTheme();
 
 const Homepage = () => {
-    const divStyle = {
-        paddingTop: "200px",
-        paddingBotton: "200px",
-    };
 
-    const h1Style = {
-        color: "blue",
-        padding: "30px",
-    }
-
-    const linkStyle = {
-        color: "black",
-        padding: "30px"
-    }
-
-    const statusStyle = {
-        paddingLeft: "100px",
-    }
-
-
-    // prints out the status of the user 
-    function status(currentUser) {
-        if(currentUser.isLoggedIn) {
-            return (<h2 style = { statusStyle }>Welcome back {currentUser.username}</h2>)
-        }
-        else {
-            return (<h2 style = { statusStyle }>You're not logged in :(</h2>)
-        }
-    }
-
-
-    const currentUser = useSelector(selectCurrentUser);
-    console.log(currentUser);
 
     return (
-        <>
-            <Header />
-             <div style={divStyle} >
-                <h1 style={h1Style}>Welcome to Media Cloud</h1>
-                {status(currentUser)} 
-            </div> 
 
-            <Login />
-        </>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <Header />
+                <Login />
+            </Container>
+        </ThemeProvider>
+
 
     );
 }
