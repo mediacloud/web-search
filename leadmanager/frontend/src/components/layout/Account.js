ba74b0a355637285fbd01ba054539e344fa08364
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-22 19:44:28 -0400

    link

--- leadmanager/frontend/src/components/layout/Account.js
+++ leadmanager/frontend/src/components/layout/Account.js
@@ -1,7 +1,13 @@
 import React from "react";
+
+//getting information from store
 import { selectCurrentUser } from '../../services/userApi';
 import { useSelector } from 'react-redux';
 
+
+// router
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Account = () => {
@@ -13,10 +19,10 @@ const Account = () => {
     }
     return (
         <>
-            <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
-            <h2 style={fontStyle}>Email: {currentUser.email}</h2>
-            <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
-            <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
+                <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
+                <h2 style={fontStyle}>Email: {currentUser.email}</h2>
+                <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
+                <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
         </>
     );
 }
