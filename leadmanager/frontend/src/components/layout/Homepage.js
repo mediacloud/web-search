5d60449c93d9a8dad2190090973144ebc571f2a6
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-22 21:35:55 -0400

    commenting, removed account because it's linked, removed unused imports

--- leadmanager/frontend/src/components/layout/Homepage.js
+++ leadmanager/frontend/src/components/layout/Homepage.js
@@ -1,39 +1,30 @@
-import React, { Component, Fragment } from 'react'
-import { Outlet, Link } from 'react-router-dom';
+// React 
+import React from 'react'
+
+// Componenets 
 import Header from './Header';
-import Login from './Login'
-import Account from './Account';
+
+// MUI styling 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
 const theme = createTheme();
 
 
-// Get user information 
-import { useSelector } from 'react-redux';
-import { selectCurrentUser } from '../../services/userApi';
-
 
 
 
 const Homepage = () => {
 
-    const currentUser = useSelector(selectCurrentUser);
 
     return (
 
         <ThemeProvider theme={theme}>
             <Container component="main" maxWidth="xs">
+
                 <Header />
 
-                <div style={{ paddingTop: "200px" }}>
-                    <Account />
-                </div>
-
             </Container>
-
-
-
         </ThemeProvider>
 
     );
