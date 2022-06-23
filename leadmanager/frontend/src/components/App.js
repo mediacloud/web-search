f8946f59dacfd0ecb72276d53857327de06e496e
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-22 22:21:23 -0400

    routes for explorer, topic mapper, adn source manager

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -8,9 +8,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 // Componenets
-import Header from './layout/Header';
 import Homepage from './layout/Homepage';
 import Account from './layout/Account';
+
+import Explorer from './pages/Explorer'
+import SourceMananger from './pages/SourceManager'
+import TopicMapper from './pages/TopicMapper'
+
 
 // Store 
 import { getStore } from '../store';
@@ -27,7 +31,10 @@ export const renderApp = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App />}>
-                            <Route path = "/Account" element= {<Account />} />
+                        <Route path="Explorer" element={<Explorer />} />
+                        <Route path="TopicMapper" element={<TopicMapper />} />
+                        <Route path="SourceManager" element={<SourceMananger />} />
+                        <Route path="Account" element={<Account />} />
                     </Route>
                 </Routes>
             </BrowserRouter >
