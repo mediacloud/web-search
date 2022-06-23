e036062733b37d69822e6642ae09445d6e637f5d
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-23 12:09:44 -0400

    theme and signup

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -14,6 +14,7 @@ import Homepage from './layout/Homepage';
 // user status 
 import Account from './layout/Account'
 import Login from './layout/Login'
+import SignUp from './SignUp';
 
 // pages 
 import Explorer from './pages/Explorer'
@@ -21,12 +22,28 @@ import SourceMananger from './pages/SourceManager'
 import TopicMapper from './pages/TopicMapper'
 
 
+
+// MUI Styling 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+
+const theme = createTheme();
 // Store 
 import { getStore } from '../store';
 
 const App = () => (
     <Provider store={getStore()}>
-        <Homepage />
+
+        {/* <Homepage /> */}
+
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+
+                <SignUp />
+                
+            </Container>
+        </ThemeProvider>
+
     </Provider >
 );
 
@@ -39,7 +56,7 @@ export const renderApp = () => {
                         <Route path="Explorer" element={<Explorer />} />
                         <Route path="TopicMapper" element={<TopicMapper />} />
                         <Route path="SourceManager" element={<SourceMananger />} />
-                        
+
                         <Route path="Login" element={<Login />} />
                         <Route path="Account" element={<Account />} />
                     </Route>
