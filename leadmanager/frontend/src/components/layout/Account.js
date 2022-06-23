d6811f6fea931b1de2ff31b01927947966d1646f
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-22 21:33:04 -0400

    styling

--- leadmanager/frontend/src/components/layout/Account.js
+++ leadmanager/frontend/src/components/layout/Account.js
@@ -1,13 +1,10 @@
+// React
 import React from "react";
 
-//getting information from store
+// information from store
 import { selectCurrentUser } from '../../services/userApi';
 import { useSelector } from 'react-redux';
 
-
-// router
-
-import { useNavigate } from "react-router-dom";
 
 const Account = () => {
     const currentUser = useSelector(selectCurrentUser);
@@ -17,16 +14,14 @@ const Account = () => {
         fontFamily: 'Courier',
     }
     return (
-        <>
 
+        <div style={{ paddingTop: "200px" }}>
+            <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
+            <h2 style={fontStyle}>Email: {currentUser.email}</h2>
+            <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
+            <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
+        </div>
 
-            <div style={{ paddingTop: "200px" }}>
-                <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
-                <h2 style={fontStyle}>Email: {currentUser.email}</h2>
-                <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
-                <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
-            </div>
-        </>
     );
 }
 
