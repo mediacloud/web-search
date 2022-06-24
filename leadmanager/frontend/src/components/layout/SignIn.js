a7e834bdfa0713347bfed543d20fda999275ff2c
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-24 11:08:12 -0400

    deleted link from mui and imported link from react-router-dom

--- leadmanager/frontend/src/components/layout/SignIn.js
+++ leadmanager/frontend/src/components/layout/SignIn.js
@@ -5,7 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -13,6 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import { Link } from 'react-router-dom';
 
 const theme = createTheme();
 
@@ -80,14 +81,15 @@ export default function SignIn() {
                     </Button>
                     <Grid container>
                         <Grid item xs>
-                            <Link href="#" variant="body2">
+                            {/* <Link href="#" variant="body2">
                                 Forgot password?
-                            </Link>
+                            </Link> */}
                         </Grid>
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            {/* <Link href="#" variant="body2">
                                 {"Don't have an account? Sign Up"}
-                            </Link>
+                            </Link> */}
+                            
                         </Grid>
                     </Grid>
                 </Box>
