66fef0c815a95dc8f66c08262e0daa48bda124f4
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-27 10:16:03 -0400

    search feature

---
+++ leadmanager/frontend/src/components/pages/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Explorer extends Component {
+  render() {
+    return (
+      <h1 style={{paddingTop: "200px"}}>Search</h1>
+    )
+  }
+}
+
+export default Explorer
