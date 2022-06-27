60169ecd30d787ad1659f6a4b8a6d3d1cc124ef7
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-27 10:14:46 -0400

    only two tools used

---
+++ leadmanager/frontend/src/components/pages/Collection.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export class SourceManager extends Component {
+  render() {
+    return (
+      <h1 style={{ paddingTop: "200px" }}>Collection</h1>    )
+  }
+}
+
+export default SourceManager
