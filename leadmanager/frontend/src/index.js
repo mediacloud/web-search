9831798054aac044d23e5b5acf3e5ff299a2b116
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-28 19:21:49 -0400

    comments

--- leadmanager/frontend/src/index.js
+++ leadmanager/frontend/src/index.js
@@ -2,27 +2,14 @@ import { renderApp } from './components/App';
 import { getStore } from './store';
 import { userApiSlice } from './services/userApi';
 
-//import { currentUserProfileAction } from './actions/user';
-//import { useProfileQuery }
-// First check if they are logged in already
-// dispatch a Redux action that calls /server/login-with-cookie
-// on server: check if a user is logged in, if so then return user object, if not then return 401
-// if server returns user object, save it to store in a useful place and set a `isLoggedIn` on the store to true (it should default false)
-// then call renderApp
-
-
-//store.dispath(currentUserProfile()).then(() => renderApp());
-//currentUserProfile is a redux
-
-
-// const currentUserProfile = () => { return fetch(‘/auth/login-with-cookie’).then(res => res.json())}
-//fetch(url, credentials:’include’)
+// gets the store from store.js 
+// dispatches the current query from userApi
+// renders app from App.js (holds all the routing)
 
 const initializeApp = () => {
   const store = getStore();
-  //store.dispatch(currentUserProfileAction());
   store.dispatch(
-     userApiSlice.endpoints.current.initiate()
+     userApiSlice.endpoints.current.initiate(),
   ).then(() => renderApp());
 }
 
