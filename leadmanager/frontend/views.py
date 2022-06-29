cf479e8d38311318d4b3c26d7d83a1386b35a708
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-29 09:33:12 -0400

    logout function

--- leadmanager/frontend/views.py
+++ leadmanager/frontend/views.py
@@ -1,7 +1,14 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import auth
 
 # views are python functions or classes that 
 # recieve a web request and return a web response 
 def index(request):
   return render(request, 'frontend/index.html')
+
+
+
+def logout(request):
+  auth.logout(request);
+  return redirect('/')
