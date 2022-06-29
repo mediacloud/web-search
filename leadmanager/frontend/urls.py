eaf52dcfe0ea421e0f75f1a7d34b1bf5f7fcaf2d
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-29 09:32:31 -0400

    added accounts/logout

--- leadmanager/frontend/urls.py
+++ leadmanager/frontend/urls.py
@@ -8,8 +8,10 @@ from . import views
 # tells django to search for URL patterns 
 # responsible for mapping the routes and paths in your project 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="home"),
     path('admin', admin.site.urls),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('accounts/logout', views.logout, name='logout')
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
