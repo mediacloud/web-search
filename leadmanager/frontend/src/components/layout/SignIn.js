8580ef88fbd082d7631548097bb50b90d3def3d9
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-01 11:12:12 -0400

    working on front end and trying to connect to back end

--- leadmanager/frontend/src/components/layout/SignIn.js
+++ leadmanager/frontend/src/components/layout/SignIn.js
@@ -75,24 +75,11 @@ export default function SignIn() {
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
+                        onClick = {console.log("hello")}
                     >
                         Sign In
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            {/* <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link> */}
-                        </Grid>
-                        <Grid item>
-                            {/* <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link> */}
-
-                            {/* <Link to="SignUp">Don't have an account? Sign Up</Link> */}
-                            
-                        </Grid>
-                    </Grid>
+                    
                 </Box>
             </Box>
         </div>
