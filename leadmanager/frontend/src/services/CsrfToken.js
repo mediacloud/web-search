e2b21fa3c383cd1f25e21c4b3c0d4a0ae23e0693
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-01 12:32:35 -0400

    token

---
+++ leadmanager/frontend/src/services/CsrfToken.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react'
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+export class csrfToken extends Component {
+  render() {
+    return (
+      <div> Hello
+      </div>
+    )
+  }
+}
+
+export default csrfToken
+
+
