e1acd4c2f8b42c86264fe2a75e41bbcdb2b448a0
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-01 12:15:37 -0400

    working on changing sign in for simplicity purposes

--- leadmanager/frontend/src/components/layout/SignIn.js
+++ leadmanager/frontend/src/components/layout/SignIn.js
@@ -27,62 +27,94 @@ export default function SignIn() {
         });
     };
 
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
     return (
 
-        <div style={{paddingTop: "100px"}}>
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    {/* <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    /> */}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                        onClick = {console.log("hello")}
-                    >
-                        Sign In
-                    </Button>
-                    
-                </Box>
-            </Box>
-        </div>
+        <div style={{ paddingTop: "100px" }}>
+            <form
+                 action='accounts/login'
+                 method='post'
+                 href='accounts/login'
+            
+                >
+                <input type="text" name='username' placeholder='username' />
+                <input type="password" name='password' placeholder='password' />
+                <input type="Submit"></input>
+
+            </form>
+        </div >
+
+        // <div style={{ paddingTop: "100px" }}>
+        //     <CssBaseline />
+        //     <Box
+        //         sx={{
+        //             marginTop: 8,
+        //             display: 'flex',
+        //             flexDirection: 'column',
+        //             alignItems: 'center',
+        //         }}
+        //     >
+        //         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        //             <LockOutlinedIcon />
+        //         </Avatar>
+        //         <Typography component="h1" variant="h5">
+        //             Sign in
+        //         </Typography>
+        //         <Box
+        //             component="form"
+        //             onSubmit={handleSubmit}
+        //             noValidate sx={{ mt: 1 }}>
+
+        //             <TextField
+        //                 margin="normal"
+        //                 required
+        //                 fullWidth
+        //                 id="email"
+        //                 label="Email Address"
+        //                 name="email"
+        //                 autoComplete="email"
+        //                 autoFocus
+        //             />
+        //             <TextField
+        //                 margin="normal"
+        //                 required
+        //                 fullWidth
+        //                 name="password"
+        //                 label="Password"
+        //                 type="password"
+        //                 id="password"
+        //                 autoComplete="current-password"
+        //             />
+        //             {/* <FormControlLabel
+        //                 control={<Checkbox value="remember" color="primary" />}
+        //                 label="Remember me"
+        //             /> */}
+        //             <Button
+        //                 type="submit"
+        //                 fullWidth
+        //                 variant="contained"
+        //                 sx={{ mt: 3, mb: 2 }}
+        //             >
+        //                 Sign In
+        //             </Button>
+
+        //         </Box>
+        //     </Box>
+        // </div>
 
     );
 }
