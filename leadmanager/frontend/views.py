0abe09a100928bc4fdae3db18ce3161c64ae1c21
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-01 11:12:21 -0400

    login:

--- leadmanager/frontend/views.py
+++ leadmanager/frontend/views.py
@@ -1,17 +1,31 @@
 
+from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import auth
 
-# views are python functions or classes that 
-# recieve a web request and return a web response 
-def index(request):
-  return render(request, 'frontend/index.html')
+# views are python functions or classes that
+# recieve a web request and return a web response
 
+
+def index(request):
+    return render(request, 'frontend/index.html')
 
 
 def logout(request):
-  auth.logout(request);
-  return redirect('/')
+    auth.logout(request)
+    return redirect('/')
+
 
 def login(request):
-  pass
+    if(request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+          auth.login(request,user)
+          return redirect("/")
+        else: 
+          messages.info(request, 'invalid credentials')
+          return redirect('login')
