fd8fb61f9feb8a74fae44fbea1c2975edd7bf447
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-05 14:40:02 -0400

    register

--- leadmanager/frontend/urls.py
+++ leadmanager/frontend/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
     path('accounts/logout', views.logout, name='logout'),
-    path('accounts/login',views.login, name = 'login')
+    path('accounts/login',views.login, name = 'login'),
+    path('register', views.register, name="register")
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
