de77ac614205aad1f5b146e6a88b1fad840fec70
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-05 14:40:55 -0400

    basic ui but functional, figuring out redirecting next

--- leadmanager/frontend/src/components/layout/SignUp.js
+++ leadmanager/frontend/src/components/layout/SignUp.js
@@ -14,6 +14,7 @@ import { Link } from 'react-router-dom';
 
 
 const theme = createTheme();
+import { CsrfToken } from '../../services/csrfToken';
 
 export default function SignUp() {
     const handleSubmit = (event) => {
@@ -27,88 +28,107 @@ export default function SignUp() {
 
     return (
         <div style={{ paddingTop: "100px" }}>
-
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
+            <form
+            action='register'
+            method='post'
             >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="given-name"
-                                name="firstName"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="family-name"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                            />
-                        </Grid>
 
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            {/* <Link href="#" variant="body2">
-                                Already have an account? Sign in
-                            </Link> */}
+                <CsrfToken />
 
-                            {/* <Link to="SignIn">Already have an account? Sign in</Link> */}
+                <input type='text' name='first_name' placeholder='First Name' /> <br/>
+                <input type='text' name='last_name' placeholder='Last Name' /> <br />
+                <input type='text' name='username' placeholder='Username' /> <br />
+                <input type='email' name='email' placeholder='email' /> <br />
+                <input type='password' name='password1' placeholder='Password' /> <br />
+                <input type='password' name='password2' placeholder='Confirm Password' /> <br />
+                <input type="Submit"></input>
 
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
+
+
+                
+            </form>
         </div>
     );
 }
+
+// <CssBaseline />
+//             <Box
+//                 sx={{
+//                     marginTop: 8,
+//                     display: 'flex',
+//                     flexDirection: 'column',
+//                     alignItems: 'center',
+//                 }}
+//             >
+//                 <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+//                     <LockOutlinedIcon />
+//                 </Avatar>
+//                 <Typography component="h1" variant="h5">
+//                     Sign up
+//                 </Typography>
+//                 <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+//                     <Grid container spacing={2}>
+//                         <Grid item xs={12} sm={6}>
+//                             <TextField
+//                                 autoComplete="given-name"
+//                                 name="firstName"
+//                                 required
+//                                 fullWidth
+//                                 id="firstName"
+//                                 label="First Name"
+//                                 autoFocus
+//                             />
+//                         </Grid>
+//                         <Grid item xs={12} sm={6}>
+//                             <TextField
+//                                 required
+//                                 fullWidth
+//                                 id="lastName"
+//                                 label="Last Name"
+//                                 name="lastName"
+//                                 autoComplete="family-name"
+//                             />
+//                         </Grid>
+//                         <Grid item xs={12}>
+//                             <TextField
+//                                 required
+//                                 fullWidth
+//                                 id="email"
+//                                 label="Email Address"
+//                                 name="email"
+//                                 autoComplete="email"
+//                             />
+//                         </Grid>
+//                         <Grid item xs={12}>
+//                             <TextField
+//                                 required
+//                                 fullWidth
+//                                 name="password"
+//                                 label="Password"
+//                                 type="password"
+//                                 id="password"
+//                                 autoComplete="new-password"
+//                             />
+//                         </Grid>
+
+//                     </Grid>
+//                     <Button
+//                         type="submit"
+//                         fullWidth
+//                         variant="contained"
+//                         sx={{ mt: 3, mb: 2 }}
+//                     >
+//                         Sign Up
+//                     </Button>
+//                     <Grid container justifyContent="flex-end">
+//                         <Grid item>
+//                             {/* <Link href="#" variant="body2">
+//                                 Already have an account? Sign in
+//                             </Link> */}
+
+//                             {/* <Link to="SignIn">Already have an account? Sign in</Link> */}
+
+//                         </Grid>
+//                     </Grid>
+//                 </Box>
+//             </Box>
