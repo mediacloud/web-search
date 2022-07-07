8645109003d9afa3f0322b36f65967b79129a908
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-07 11:39:04 -0400

    finalized sign in with mui

--- leadmanager/frontend/src/components/layout/SignIn.js
+++ leadmanager/frontend/src/components/layout/SignIn.js
@@ -3,100 +3,91 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import { Link } from 'react-router-dom';
 
 const theme = createTheme();
 
-import CsrfToken from '../../services/CsrfToken'
+import { CsrfToken } from '../../services/CsrfToken'
 
 export default function SignIn() {
 
-
-    {/* <input type="hidden" name="csrfmiddlewaretoken" value={getCookie('csrftoken')} /> */ }
-
-
-    console.log({CsrfToken})
-
-
     return (
+        <>
+            <div style={{ paddingTop: "100px" }}>
+                <CssBaseline />
 
-        <div style={{ paddingTop: "100px" }}>
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
                 <Box
-                    component="form"
-                    action='accounts/login'
-                    method='post'
-                    href='accounts/login'
-                    noValidate sx={{ mt: 1 }}
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
                 >
 
-
-
-                    {/* Token  */}
-                    {/* <CsrfToken /> */}
-
-                    {/* Username  */}
-
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="text"
-                        label="Username"
-                        name="username"
-                        autoComplete="Username"
-                        autoFocus
-                    />
-
-                    {/* Password  */}
-
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        action='accounts/login'
+                        method='post'
+                        href='accounts/login'
+                        noValidate sx={{ mt: 1 }}
                     >
-                        Sign In
-                    </Button>
 
+
+
+                        {/* Token  */}
+                        <CsrfToken />
+
+
+                        {/* Username  */}
+
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="text"
+                            label="Username"
+                            name="username"
+                            autoComplete="Username"
+                            autoFocus
+                        />
+
+                        {/* Password  */}
+
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+
+                    </Box>
                 </Box>
-            </Box>
-        </div>
+            </div>
+        </>
 
     );
 }
