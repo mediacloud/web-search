a966e080e5ffd8a2eb46a8b0ba35de9fe8b571be
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-09 20:27:20 -0400

    commiting before pulling

--- leadmanager/frontend/src/components/layout/Account.js
+++ leadmanager/frontend/src/components/layout/Account.js
@@ -7,22 +7,22 @@ import { useSelector } from 'react-redux';
 
 
 const Account = () => {
-    const currentUser = useSelector(selectCurrentUser);
+  const currentUser = useSelector(selectCurrentUser);
 
 
-    const fontStyle = {
-        fontFamily: 'Courier',
-    }
-    return (
+  const fontStyle = {
+    fontFamily: 'Courier',
+  }
+  return (
 
-        <div style={{ paddingTop: "200px" }}>
-            <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
-            <h2 style={fontStyle}>Email: {currentUser.email}</h2>
-            <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
-            <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
-        </div>
+    <div style={{ paddingTop: "200px" }}>
+      <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
+      <h2 style={fontStyle}>Email: {currentUser.email}</h2>
+      <h2 style={fontStyle}>isStaff: {currentUser.is_staff.toString()}</h2>
+      <h2 style={fontStyle}>isSuperUser: {currentUser.is_superuser.toString()}</h2>
+    </div>
 
-    );
+  );
 }
 
 export default Account
