43eeda42f9e075e3ffbf82c28dd20a34fa9f14a0
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-11 19:00:37 -0400

    deleted register

--- leadmanager/frontend/views.py
+++ leadmanager/frontend/views.py
@@ -13,39 +13,3 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'frontend/index.html')
-
-
-def register(request):
-
-    if(request.method == 'POST'):
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        email = request.POST['email']
-
-        
-        if password1 == password2:
-            
-            if User.objects.filter(username=username).exists():
-                logging.debug('Username taken')
-                messages.info(request, 'Username taken')
-                return redirect('/')
-            
-            elif User.objects.filter(email=email).exists():
-                logging.debug('Email Exists')
-                messages.info(request, 'Email Taken')
-                return redirect('/')
-            
-            else:
-                logging.debug('User Created')
-                user = User.objects.create_user(
-                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-                user.save()
-        else:
-            logging.debug('password not matching')
-            messages.info(request, 'password not matching')
-            return redirect('/')
-        return redirect('/')
-    return render(request, 'frontend/index.html')
