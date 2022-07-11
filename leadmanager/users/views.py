176e3a25db12f3064b86c18b28d51c08eb8dd0fb
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-11 18:59:51 -0400

    register function

--- leadmanager/users/views.py
+++ leadmanager/users/views.py
@@ -34,6 +34,54 @@ def login(request):
         return HttpResponse(status=405)
 
 
+def register(request):
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+
+        first_name = payload.get('first_name', None)
+        last_name = payload.get('last_name', None)
+        email = payload.get('email', None)
+        username = payload.get('username', None)
+        password1 = payload.get('password1', None)
+        password2 = payload.get('password1', None)
+
+        user = auth.authenticate(first_name=first_name,
+                                 last_name=last_name,
+                                 email=email,
+                                 username=username,
+                                 password1=password1,
+                                 password2=password2)
+
+        if password1 == password2:
+
+            if user.objects.filter(username=username).exists():
+                logger.debug('Username taken')
+                data = json.dumps({'message': "Username taken"})
+                return HttpResponse(data, content_type='application/json', status=403)
+
+            elif user.objects.filter(email=email).exists():
+                logger.debug('Email taken')
+                data = json.dumps({'message': "Email taken"})
+                return HttpResponse(data, content_type='application/json', status=403)
+
+            else:  # doesn't login because not superuser
+                logger.debug('user created')
+
+                created_user = user.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+
+                created_user.save()
+
+                data = json.dumps({'message': "user created"})
+                return HttpResponse(data, content_type='application/json', status=200)
+        else:
+            logging.debug('password not matching')
+            data = json.dumps({'message': "password not matching"})
+            return HttpResponse(data, content_type='application/json', status=403)
+    else:
+        return HttpResponse(status=405)
+
+
 def logout(request):
     logging.debug('logout success')
     auth.logout(request)
