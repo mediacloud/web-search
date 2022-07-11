b942717984dbc9872cb022009ff1b6c6b6d3318d
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-11 19:57:23 -0400

    mirrored signin.js

--- leadmanager/frontend/src/features/auth/SignUp.js
+++ leadmanager/frontend/src/features/auth/SignUp.js
@@ -10,123 +10,161 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { CsrfToken } from '../../services/csrfToken';
+import { useRegisterMutation } from '../../app/services/authApi';
+import { setCredentials } from './authSlice';
+
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function SignUp() {
 
-    return (
-        
-        <div style={{ paddingTop: "100px" }}>
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <Box
-                    component="form"
-                    action='register'
-                    method='post'
-                    noValidate
-                    sx={{ mt: 3 }}
-                >
-                    <Grid container spacing={2}>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-                        {/* Token  */}
-                        <CsrfToken />
+  // formstate -> login  
+  const [register, { isLoading }] = useRegisterMutation();
+
+  // username and password
+  const [formState, setFormState] = React.useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+
+  // errors 
+  const [errorState, setErrorState] = React.useState();
+
+  return (
+
+    <div style={{ paddingTop: "100px" }}>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          sx={{ mt: 3 }}
+        >
+
+          {errorState && <Alert severity="error">Failed to sign in</Alert>}
 
 
-                        {/* First Name */}
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="First Name"
-                                name="first_name"
-                                autoComplete="given-name"
-                            />
-                        </Grid>
+          <Grid container spacing={2}>
 
-                        {/* Last Name */}
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="Last Name"
-                                name="last_name"
-                                type="name"
-                                autoComplete="family-name"
-                            />
-                        </Grid>
+            {/* Token  */}
+            <CsrfToken />
 
-                        {/* Email */}
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="Email Address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
 
-                        {/* Username */}
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="Username"
-                                name="username"
-                                type="text"
-                                autoComplete="username"
-                            />
-                        </Grid>
+            {/* First Name */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                label="First Name"
+                name="first_name"
+                autoComplete="given-name"
+                autoFocus
+              />
+            </Grid>
 
-                        {/* Password */}
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password1"
-                                label="Password"
-                                type="password"
-                                autoComplete="new-password"
-                            />
-                        </Grid>
+            {/* Last Name */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                label="Last Name"
+                name="last_name"
+                type="name"
+                autoComplete="family-name"
+              />
+            </Grid>
 
-                        {/* Confirm Password */}
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password2"
-                                label="Confirm Password"
-                                type="password"
-                                autoComplete="new-password"
-                            />
-                        </Grid>
+            {/* Email */}
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="Email Address"
+                name="email"
+                type="email"
+                autoComplete="email"
+              />
+            </Grid>
 
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign Up
-                    </Button>
-                </Box>
-            </Box>
-        </div>
+            {/* Username */}
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                name="username"
+                type="text"
+                autoComplete="username"
+              />
+            </Grid>
 
-    );
+            {/* Password */}
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password1"
+                label="Password"
+                type="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+
+            {/* Confirm Password */}
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password2"
+                label="Confirm Password"
+                type="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+
+          </Grid>
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={isLoading}
+            onClick={async () => {
+              try {
+                setErrorState(null);
+                const user = await register(formState).unwrap();
+                dispatch(setCredentials(user));
+                navigate("/")
+              } catch (err) {
+                console.log(err);
+                setErrorState(err.data.message);
+              }
+            }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </div >
+
+  );
 }
