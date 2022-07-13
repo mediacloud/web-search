cfd68d34b6e0592258f77772304e888cae47ff7c
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-13 15:23:16 -0400

    from (date picker) to (date picker) w/ mui

--- leadmanager/frontend/src/features/search/Search.js
+++ leadmanager/frontend/src/features/search/Search.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
-export class Explorer extends Component {
-  render() {
-    return (
-      <h1 style={{paddingTop: "200px"}}>Search</h1>
-    )
-  }
+export default function MaterialUIPickers() {
+  const [value, setValue] = React.useState();
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div style={{paddingTop : "200px"}}>
+
+
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={2}>
+          <DesktopDatePicker
+            label="From"
+            inputFormat="MM/dd/yyyy"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <DesktopDatePicker
+            label="To"
+            inputFormat="MM/dd/yyyy"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
+    </div>
+  );
 }
-
-export default Explorer
