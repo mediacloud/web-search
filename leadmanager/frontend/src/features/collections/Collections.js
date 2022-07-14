2a3a18f81054409226bdadf994eef134d10a9ff4
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-14 19:51:29 -0400

    renamed

---
+++ leadmanager/frontend/src/features/collections/Collections.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { selectIsLoggedIn } from '../auth/authSlice'
+
+
+export default function Collections() {
+  return (
+    <div style={{ paddingTop: "200px" }}> Collections </div>
+  )
+}
