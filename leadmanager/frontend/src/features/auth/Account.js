d063c8d0395e63dadb50dacd6e177c2e2f660bff
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-14 18:46:15 -0400

    updated

--- leadmanager/frontend/src/features/auth/Account.js
+++ leadmanager/frontend/src/features/auth/Account.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // information from store
-import { selectCurrentUser } from './authSlice';
+import { selectCurrentUser } from '../../features/auth/authSlice';
 import { useSelector } from 'react-redux';
 
 
@@ -10,13 +10,14 @@ const Account = () => {
   const currentUser = useSelector(selectCurrentUser);
   const fontStyle = {
     fontFamily: 'Courier',
+    fontSize: "18px"
   }
   return (
     <div style={{ paddingTop: "200px" }}>
-      <h1 style={fontStyle}>Account Username: {currentUser.username} </h1>
-      <h2 style={fontStyle}>Email: {currentUser.email}</h2>
-      <h2 style={fontStyle}>isStaff: {currentUser.isStaff.toString()}</h2>
-      <h2 style={fontStyle}>isSuperUser: {currentUser.isSuperuser.toString()}</h2>
+      <h1 style={fontStyle}>Account Username: {currentUser.username}</h1>
+      <h1 style={fontStyle}>Email: {currentUser.email}</h1>
+      <h1 style={fontStyle}>isStaff: {currentUser.isStaff.toString()}</h1>
+      <h1 style={fontStyle}>isSuperUser: {currentUser.isSuperuser.toString()}</h1>
     </div>
   );
 }
