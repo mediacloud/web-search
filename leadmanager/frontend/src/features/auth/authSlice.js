8b153959deeab0ced2b4107f6cd67d942c67aa3e
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-22 15:12:46 -0400

    setSearch slice

--- leadmanager/frontend/src/features/auth/authSlice.js
+++ leadmanager/frontend/src/features/auth/authSlice.js
@@ -2,16 +2,21 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const slice = createSlice({
   name: 'auth',
-  initialState: { user: null, isLoggedIn: false },
+  initialState: { search: null, user: null, isLoggedIn: false },
   reducers: {
     setCredentials: (state, { payload }) => {
       state.user = payload;
       state.isLoggedIn = state.user && state.user.isActive;
     },
+    setSearch: (state, { payload }) => {
+      state.search = payload;
+    }
   },
 });
 
 export const setCredentials = slice.actions.setCredentials;
+
+export const setSearch = slice.actions.setSearch
 
 export const selectCurrentUser = (state) => state.auth.user;
 
