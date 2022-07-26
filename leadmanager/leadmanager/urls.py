5190a393c680e450e54481560eb1f3151bf88645
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-26 12:12:17 -0400

    deleted

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     path('', include('frontend.urls')),
     path('admin', admin.site.urls),
-    path('api/auth/', include('users.urls'))
+    path('api/auth/', include('backend.users.urls')),
+    path('api/search/', include('backend.search.urls')),
+    path('api/collections/', include('backend.collections.urls'))
 ]
