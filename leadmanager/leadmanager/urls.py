395da3bed2c1b5fec833b2306aec1f6373e2c444
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-26 13:45:22 -0400

    added url for search folder

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('admin', admin.site.urls),
     path('api/auth/', include('users.urls')),
+    path('api/search/', include('search.urls')),
 ]
