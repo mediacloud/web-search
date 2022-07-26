b2e6b032b3bdc5d420b36d4715ba35aade197346
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-26 13:44:04 -0400

    new folder for search

--- leadmanager/frontend/src/app/store.js
+++ leadmanager/frontend/src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { api as authApi } from './services/authApi';
-import { searchApi } from '../features/search/apiSearch'
-import authReducer, { setCredentials } from '../features/auth/authSlice';
+import authReducer from '../features/auth/authSlice';
 
 let store; // singleton store
 
@@ -19,14 +18,14 @@ const setupStore = () => {
     reducer: {
       // Add the generated reducer as a specific top-level slice
       [authApi.reducerPath]: authApi.reducer,
-      auth: setCredentials, setSearch
+      auth: authReducer,
     },
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
-    middleware: (getDefaultMiddleWare) => 
-    getDefaultMiddleWare().concat(searchApi.middleware)
-    //userApiSlice.middleware,
-
+    middleware: [
+      ...getDefaultMiddleware(),
+      //userApiSlice.middleware,
+    ],
   });
   // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
   // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
