957c5c4e042e133582fb51c1d42bbd4a173c6be6
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-27 11:39:26 -0400

    deleted

--- leadmanager/backend/collections/models.py
+++ leadmanager/backend/collections/models.py
@@ -4,6 +4,5 @@ class Collection(models.Model):
     id = models.IntegerField()
     name = models.CharField()
     notes = models.CharField()
-    mc_tags_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add= True)
     modified_at = models.DateTimeField(modified_at = True)
