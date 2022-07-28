ee7a5315e8d3a34a14f2a76124351c2de1d39fc6
Rahul Bhargava <rahulbot@gmail.com>
2022-07-28 13:02:05 -0400

    force setting of CSRF token
    don't know why it wasn't setting, but lets force it to so form submissions work

--- leadmanager/frontend/views.py
+++ leadmanager/frontend/views.py
@@ -2,7 +2,7 @@ import logging
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
-
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # views are python functions or classes that
 # recieve a web request and return a web response
@@ -10,6 +10,6 @@ from django.contrib.auth.models import User, auth
 
 logger = logging.getLogger(__name__)
 
-
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'frontend/index.html')
