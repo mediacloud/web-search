7031340a775d5cccf880238bd8b663c617f65f37
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-07-28 11:01:24 -0400

    big int, deleted a line for mc_id

--- leadmanager/backend/collections/models.py
+++ leadmanager/backend/collections/models.py
@@ -1,8 +1,9 @@
 from django.db import models 
 
 class Collection(models.Model):
-    id = models.IntegerField()
+    # big int 
+    id = models.IntegerField() 
     name = models.CharField()
     notes = models.CharField()
     created_at = models.DateTimeField(auto_now_add= True)
-    modified_at = models.DateTimeField(modified_at = True)
+    modified_at = models.DateTimeField(modified_at= True)
