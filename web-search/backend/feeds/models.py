1d864f906d2f035f259d3dcc016cffcbc2960ef3
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-01 12:38:36 -0400

    auto_now = True

--- web-search/backend/feeds/models.py
+++ web-search/backend/feeds/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Feeds(models.Model):
     # big int
-    id = models.BigIntegerField()
-    url = models.TextField()
+    default_auto_field = models.BigAutoFeild()
+    url = models.CharField()
     admin_rss_enabled = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(modified_at=True)
+    modified_at = models.DateTimeField(auto_now=True)
