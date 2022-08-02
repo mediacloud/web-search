65c333083b32da39ad13d168a7bf805af239dedc
Rahul Bhargava <rahulbot@gmail.com>
2022-08-02 11:34:15 -0400

    auto-download gzipped data files for import

--- mcweb/backend/sources/management/commands/importdata.py
+++ mcweb/backend/sources/management/commands/importdata.py
@@ -1,26 +1,40 @@
 from django.core.management.base import BaseCommand, CommandError
 import os
 from subprocess import call
+import glob
+import tempfile
 
 from ...models import Collection, Source, Feed, ServiceNames
+
+
+def _run_psql_command(cmd: str):
+    db_uri = os.getenv('DATABASE_URI')
+    call(['psql', '-Atx', db_uri, '-c', cmd])
 
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
-    def _run_command(self, cmd: str):
-        db_uri = os.getenv('DATABASE_URI')
-        call(['psql', '-Atx', db_uri, '-c', cmd])
-
-    def add_arguments(self, parser):
-        # Positional arguments
-        parser.add_argument('dir', type=str)
-
     def handle(self, *args, **options):
+        file_dir = tempfile.gettempdir()  # prints the current temporary directory
+        self.stdout.write(self.style.SUCCESS('Importing from "%s"' % file_dir))
+
+        # download the files
+        urls = [
+            'https://mediacloud-media-merge.s3.amazonaws.com/coll.csv.gz',
+            'https://mediacloud-media-merge.s3.amazonaws.com/coll-sources.csv.gz',
+            'https://mediacloud-media-merge.s3.amazonaws.com/sources.csv.gz',
+            'https://mediacloud-media-merge.s3.amazonaws.com/feeds.csv.gz'
+        ]
+        for u in urls:
+            call(['wget', u, '-P', file_dir])
+
+        # unzip them all
+        zipfiles = glob.glob("{}/*.gz".format(file_dir))
+        for z in zipfiles:
+            call(['gunzip', z])
 
         # validate inputs
-        file_dir = options['dir']
-        self.stdout.write(self.style.SUCCESS('Importing from "%s"' % file_dir))
         sources_path = os.path.join(file_dir, 'sources.csv')
         if not os.path.exists(sources_path):
             raise CommandError("Can't find file %s" % sources_path)
@@ -35,37 +49,37 @@ class Command(BaseCommand):
             raise CommandError("Can't find file %s" % coll_src_links_path)
 
         # wipe and import Sources
-        """
         self.stdout.write(self.style.SUCCESS('Importing sources'))
         Source.objects.all().delete()
         cmd = "\\copy sources_source (id, name, url_search_string, label, homepage, notes, service, pub_country," \
               "pub_state, primary_language, media_type) from " \
               "'import-data/sources.csv' CSV QUOTE '\"' HEADER".format(sources_path)
-        self._run_command(cmd)
-        self._run_command("UPDATE sources_source SET created_at=NOW(), modified_at=NOW()")
-        self._run_command("UPDATE sources_source SET primary_language=NULL WHERE primary_language='none'")
+        _run_psql_command(cmd)
+        _run_psql_command("UPDATE sources_source SET created_at=NOW(), modified_at=NOW(), service='{}'".format(
+            ServiceNames.ONLINE_NEWS))
+        _run_psql_command("UPDATE sources_source SET primary_language=NULL WHERE primary_language='none'")
 
         # wipe and import Feeds
         self.stdout.write(self.style.SUCCESS('Importing feeds'))
         Feed.objects.all().delete()
         cmd = "\\copy sources_feed (id,source_id,name,url) from 'import-data/feeds.csv' CSV QUOTE '\"' HEADER".\
             format(feeds_path)
-        self._run_command(cmd)
-        self._run_command("UPDATE sources_feed SET created_at=NOW(), modified_at=NOW(), admin_rss_enabled=True, "
-                          "service={}".format(ServiceNames.ONLINE_NEWS))
+        _run_psql_command(cmd)
+        _run_psql_command("UPDATE sources_feed SET created_at=NOW(), modified_at=NOW(), admin_rss_enabled=True")
 
         # wipe and import Collections
         self.stdout.write(self.style.SUCCESS('Importing collections'))
         Collection.objects.all().delete()
-        cmd = "\\copy sources_collection (id, name, notes) from 'import-data/coll.csv' CSV QUOTE '\"' HEADER".format(collection_path)
-        self._run_command(cmd)
-        self._run_command("UPDATE sources_collection SET created_at=NOW(), modified_at=NOW()")
-        """
+        cmd = "\\copy sources_collection (id, name, notes) from 'import-data/coll.csv' CSV QUOTE '\"' HEADER".format(
+            collection_path)
+        _run_psql_command(cmd)
+        _run_psql_command("UPDATE sources_collection SET created_at=NOW(), modified_at=NOW()")
+
         # wipe and import source-collection links
         self.stdout.write(self.style.SUCCESS('Importing source-collections links'))
-        self._run_command("DELETE from sources_source_collections")
+        _run_psql_command("DELETE from sources_source_collections")
         cmd = "\\copy sources_source_collections (collection_id,source_id) from 'import-data/coll-sources.csv' " \
               "CSV QUOTE '\"' HEADER".format(coll_src_links_path)
-        self._run_command(cmd)
+        _run_psql_command(cmd)
 
         self.stdout.write(self.style.SUCCESS('Done from "%s"' % file_dir))
