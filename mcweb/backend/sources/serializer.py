65dab30e758cb2d02913100ab3c16c89d25016f2
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-04 17:11:53 -0400

    spelling mistake

--- mcweb/backend/sources/serializer.py
+++ mcweb/backend/sources/serializer.py
@@ -2,25 +2,25 @@ from rest_framework import serializers
 from .models import Collection, Feed, Source
 
 
-# Serializers in Django REST Framework are responsible for converting objects 
-# into data types understandable by javascript and 
-# front-end frameworks. Serializers also provide deserialization, 
-# allowing parsed data to be converted back into complex types, 
+# Serializers in Django REST Framework are responsible for converting objects
+# into data types understandable by javascript and
+# front-end frameworks. Serializers also provide deserialization,
+# allowing parsed data to be converted back into complex types,
 # after first validating the incoming data.
 
 class CollectionSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Collection
-        feilds = '__all__'
+        fields = '__all__'
 
 
 class FeedsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feed
-        feilds = '__all__'
+        fields = '__all__'
 
 
 class SourcesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
-        feilds = '__all__'
+        fields = '__all__'
