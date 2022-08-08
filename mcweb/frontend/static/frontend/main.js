a9ef023031e4226166ff1fc79f6b0beda141f24e
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-08 12:00:03 -0400

    main

--- mcweb/frontend/static/frontend/main.js
+++ mcweb/frontend/static/frontend/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-<<<<<<< HEAD
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js":
@@ -6143,10 +6142,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = __webpack_require__("./mcweb/frontend/src/index.js");
-=======
-/******/ 	"use strict";
-/******/ 	
->>>>>>> crudApi
 /******/ 	
 /******/ })()
 ;
