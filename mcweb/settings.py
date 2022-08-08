8a4118ae37242c8e6377f36f815ca010b9551847
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-08 12:09:42 -0400

    cleaned up

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -156,16 +156,11 @@ CACHES = {
     }
 }
 
+
+# email authentication
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_USER = "mcwebauthentication@outlook.com"
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_PASSWORD = "doixlvpyejhzxrck"
-
-# Custom setting. To email
-# RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
-RECIPIENT_ADDRESS="evanjacobsuslovich@gmail.com"
