a0b1d02258922fad14b3ad5198f9e8e72cbbe005
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-10 13:17:40 -0400

    color pallet .. created Item

--- mcweb/frontend/src/features/collections/Collections.js
+++ mcweb/frontend/src/features/collections/Collections.js
@@ -1,22 +1,40 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { palette } from '@mui/system';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function Width() {
   return (
     <div style={{ paddingTop: "100px" }}>
-      <Box sx={{ width: '100%' }}>
-        <Box
-          sx={{
-            width: 'auto',
-            p: 1,
-            bgcolor: 'blue',
-            borderRadius: 2,
-            fontSize: '0.875rem',
-            fontWeight: '700',
-            textAlign: 'center',
-          }}
-        >
-          Width auto
-        </Box>
+      <Box sx={{
+        width: '100%', height: '100vh', bgcolor: 'primary.main' }}>
+
+        <Grid container spacing={2}>
+          <Grid item md={8}>
+            <Item>Item 1</Item>
+          </Grid>
+          <Grid item md={4}>
+            <Item>Item 2</Item>
+          </Grid>
+          <Grid item md={4}>
+            <Item>Item 3</Item>
+          </Grid>
+          <Grid item md={8}>
+            <Item>Item 4</Item>
+          </Grid>
+        </Grid>
+        
+        
       </Box>
     </div>
   );
