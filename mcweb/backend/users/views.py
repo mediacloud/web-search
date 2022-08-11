681d0dd533ca526f2fb8b92f9de03de0b30d8494
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-11 19:01:15 -0400

    @login_required

--- mcweb/backend/users/views.py
+++ mcweb/backend/users/views.py
@@ -11,6 +11,7 @@ import humps
 from django.core.mail import send_mail
 import settings
 import datetime as dt
+from django.contrib.auth.decorators import login_required
 logger = logging.getLogger(__name__)
 
 # random key generator
@@ -139,6 +140,7 @@ def register(request):
         return HttpResponse(data, content_type='application/json', status=400)
 
 
+@login_required(redirect_field_name='/auth/login')
 @require_http_methods(["POST"])
 def logout(request):
     logging.debug('logout success')
