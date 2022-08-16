0af9aebb2674a222247e2bf68da43c614bc99264
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-16 12:50:14 -0400

    modify collection page

---
+++ mcweb/frontend/src/features/collections/ModifyCollection.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function ModifyCollection() {
+  return (
+    <div style={{ paddingTop: "100px" }}>
+      <h1>Modify Collection</h1>
+    </div >
+  );
+}
