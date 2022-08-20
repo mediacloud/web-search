a8787f5382b08eac7891a3b2b5d186df0a370100
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-20 14:02:03 -0400

    adding footer to homepage

--- mcweb/frontend/src/Homepage.js
+++ mcweb/frontend/src/Homepage.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import Container from '@mui/material/Container';
 import Header from './Header';
-
+import Footer from './Footer';
 const Homepage = () => {
   return (
     <Container component="main" maxWidth="xl">
       <Header />
+      <Footer />
     </Container>
   );
 }
