5098f086ddee79467186bc97b5010dd8a2848cf0
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-20 19:44:51 -0400

    needs dict to be implemented

--- mcweb/frontend/src/Footer.js
+++ mcweb/frontend/src/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const Footer = () => {
-
   return (
     <div className='footer'>
-      <h1>Footer</h1>
+      <h6>Media Cloud is a consortium research project across multiple institutions,
+        including the University of Massachusetts Amherst, Northeastern University,
+        and the Berkman Klein Center for Internet & Society at Harvard University
+      </h6>
+      <h6> v0.1.0 </h6>
     </div>
 
   );
 };
+
 export default Footer;
