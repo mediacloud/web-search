4f17355ab9e81fb22b6a170d4fda177820b05358
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-21 10:09:50 -0400

    deleted and put in index.html

--- mcweb/frontend/src/Homepage.js
+++ mcweb/frontend/src/Homepage.js
@@ -2,15 +2,10 @@
 import React from 'react'
 import Container from '@mui/material/Container';
 import Header from './Header';
-import Footer from './Footer';
 const Homepage = () => {
   return (
     <Container component="main" maxWidth="xl">
-
       <Header />
-      <div style={{paddingTop: "25px"}}>
-        <Footer />
-      </div>
     </Container>
   );
 }
