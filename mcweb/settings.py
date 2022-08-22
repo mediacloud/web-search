5d53e97ec69aaff88b4a90caa2cb7f032482b568
Evan Leon <evan.leon.j@gmail.com>
2022-08-22 11:43:38 -0400

    v0.1.1

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import environ
 
 # The static version of the app
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
