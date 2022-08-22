92a232e8effcf959268cd1bef0bcc12c38a1cf1e
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-22 17:55:12 -0400

    removing styling aka container

--- mcweb/frontend/src/Homepage.js
+++ mcweb/frontend/src/Homepage.js
@@ -1,12 +1,9 @@
 // React
 import React from 'react'
-import Container from '@mui/material/Container';
 import Header from './Header';
 const Homepage = () => {
   return (
-    <Container component="main" maxWidth="xl">
       <Header />
-    </Container>
   );
 }
 
