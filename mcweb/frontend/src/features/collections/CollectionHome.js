030a51f674e1d91b44184e46549a60419b8204ca
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-23 11:40:05 -0400

    reading data

--- mcweb/frontend/src/features/collections/CollectionHome.js
+++ mcweb/frontend/src/features/collections/CollectionHome.js
@@ -6,6 +6,8 @@ import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
 
+import collections from './media-collection.json'
+
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#99b9de' : '#fff',
     ...theme.typography.body2,
@@ -15,46 +17,71 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function CollectionHome() {
+    console.log(collections.featuredCollections)
+    collections.featuredCollections.entries.map((collection) => {
+        console.log(collection._comment)
+        collection.tags.map((tag) =>
+            console.log(tag))
+    })
     return (
         <div>
             <h1>Collections Home</h1>
             <Box sx={{ width: '100%' }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/1"}><Item>First Collection </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/2"}><Item>Second Collection </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/117"}><Item>Collection 117 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/118"}><Item>Collection 118 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Link to={"/collections/125"}><Item>Collection 125 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Link to={"/collections/129"}><Item>Collection 129 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <Link to={"/collections/142001"}><Item>Collection 142001 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/2453107"}><Item>Collection 2453107 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/7796878"}><Item>Collection 7796878 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/8875024"}><Item>Collection 8875024 </Item></Link>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} >
-                        <Link to={"/collections/8875027"}><Item>Collection 8875027 </Item></Link>
-                    </Grid>
-                </Grid>
+
             </Box>
         </div>
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <Grid container spacing={2}>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/1"}><Item>First Collection </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/2"}><Item>Second Collection </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/117"}><Item>Collection 117 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/118"}><Item>Collection 118 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Link to={"/collections/125"}><Item>Collection 125 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Link to={"/collections/129"}><Item>Collection 129 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Link to={"/collections/142001"}><Item>Collection 142001 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/2453107"}><Item>Collection 2453107 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/7796878"}><Item>Collection 7796878 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/8875024"}><Item>Collection 8875024 </Item></Link>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4} lg={3} >
+        <Link to={"/collections/8875027"}><Item>Collection 8875027 </Item></Link>
+    </Grid>
+</Grid> */}
