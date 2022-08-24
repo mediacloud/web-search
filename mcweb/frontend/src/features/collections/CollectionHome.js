3206f13121ea6ba00f099e89b49ad87891adfb08
Evan Leon <evan.leon.j@gmail.com>
2022-08-24 11:01:47 -0400

    Add box

--- mcweb/frontend/src/features/collections/CollectionHome.js
+++ mcweb/frontend/src/features/collections/CollectionHome.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import { Link } from 'react-router-dom';
 import collections from './media-collection.json'
 
