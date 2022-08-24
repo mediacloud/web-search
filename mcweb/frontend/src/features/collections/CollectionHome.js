fe3e8217edee6f3647530d5ca7c189920d56b7eb
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-24 08:58:56 -0400

    cleaned up

--- mcweb/frontend/src/features/collections/CollectionHome.js
+++ mcweb/frontend/src/features/collections/CollectionHome.js
@@ -1,15 +1,9 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import collections from './media-collection.json'
-
-import { setCollection } from './collectionsSlice';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -21,15 +15,11 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 
-
-
-
 export default function CollectionHome() {
 
     return (
         <div>
             <h1>Featured Collections</h1>
-
             {(collections.featuredCollections.entries.map((collection) => 
                 collection.tags.map((tag) =>
                     <Grid key={tag} item xs={12} sm={6} md={4} lg={3}>
