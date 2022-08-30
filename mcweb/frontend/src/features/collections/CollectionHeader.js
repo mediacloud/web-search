b36e362c6aadac1dd9196203ea78dc48fc80fa19
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-08-30 11:12:43 -0400

    added buttons with proper styling

--- mcweb/frontend/src/features/collections/CollectionHeader.js
+++ mcweb/frontend/src/features/collections/CollectionHeader.js
@@ -1,24 +1,55 @@
 import * as React from 'react';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { useGetCollectionQuery } from '../../app/services/collectionsApi';
 
-export default function CollectionHeader(props){
+export default function CollectionHeader(props) {
     const { collectionId } = props
-    const { 
+    const {
         data,
         isLoading,
         isSuccess,
         isError,
         error,
-     } = useGetCollectionQuery(collectionId);
+    } = useGetCollectionQuery(collectionId);
     const collection = data;
 
-    if (isLoading) return( <h1>Loading...</h1> )
+    if (isLoading) return (<h1>Loading...</h1>)
     else {
         return (
-            <div>
-                <h1>{collection.name}</h1>
-                <h3>Collection #{collectionId}</h3>
+            <div className='collectionHeader'>
+
+                {/* Title and Collection ID */}
+                <div className="collectionInformation">
+                    <h1 className="title">{collection.name}</h1>
+                    <h3>Collection #{collectionId}</h3>
+                </div>
+
+                {/* Buttons for Modifying and showing Sources */}
+                <div className="buttons">
+                    <Button
+                        style={{ backgroundColor: "white" }}
+                        variant='contained'
+                        sx={{ my: 2.25, color: 'black', display: 'block' }}
+                        component={Link}
+                        to="modify-collection"
+                    >
+                        Modify this Collection
+                    </Button>
+
+                    <Button
+                        style={{ backgroundColor: "white" }}
+                        variant='contained'
+                        sx={{ my: 2.25, color: 'black', display: 'block' }}
+                        onClick={async () => {
+                            setIsShown(!isShown)
+                        }}
+                    >
+                        Sources
+                    </Button>
+                </div>
+
             </div>
         )
     }
-};
+}
