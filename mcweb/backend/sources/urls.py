e4da0dd18d2da3b51c53a075b5b9eb4223a2a725
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-09-10 19:48:58 -0400

    url

--- mcweb/backend/sources/urls.py
+++ mcweb/backend/sources/urls.py
@@ -2,10 +2,18 @@ from django.urls import URLPattern
 from rest_framework import routers 
 from .api import FeedsViewSet, SourcesViewSet, CollectionViewSet, SourcesCollectionsViewSet
 
+from django.urls import path
+
+from . import views
+
 router = routers.DefaultRouter() 
 router.register('feeds', FeedsViewSet, 'feeds')
 router.register('sources', SourcesViewSet, 'sources')
 router.register('collections', CollectionViewSet, 'collections')
 router.register('sources-collections', SourcesCollectionsViewSet,'sources-collections')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('csvDownload', views.csvDownload)
+]
+
+urlpatterns += router.urls
