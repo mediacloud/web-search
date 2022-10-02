f7f7e0fc5240cf8b0e464ecd37d13d20fd70de52
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-02 15:42:05 -0400

    update

--- mcweb/frontend/src/features/search/Search.js
+++ mcweb/frontend/src/features/search/Search.js
@@ -9,6 +9,7 @@ import { useSnackbar } from 'notistack';
 import Container from '@mui/material/Container';
 
 
+import List from './List';
 import * as React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
@@ -23,158 +24,49 @@ import { useGetSearchMutation } from '../../app/services/searchApi';
 
 export default function Search() {
 
-  const [serviceList, setServiceList] = useState([
-    { service: "AI" },
-    { service: "Work"}, 
-  ])
-
-  const [negationList, setNegationList] = useState([
-    { query: "AI" },
-    { query: "Work" },
-  ])
-
-  const handleNegationAdd = () => {
-    setServiceList([...serviceList, { service: "" }])
-  }
-
-  const handleNegationRemove = (index) => {
-    const list = [...serviceList]
-    list.splice(index, 1);
-    setServiceList(list);
-  }
-
-  const handleNegationChange = (e, index) => {
-    const { name, value } = e.target
-    const list = [...serviceList];
-    list[index][name] = value;
-    setServiceList(list)
-  }
-
-
-  const handleServiceAdd = () => {
-    setServiceList([...serviceList, { service: "" }])
-  }
-
-  const handleServiceRemove = (index) => {
-    const list = [...serviceList]
-    list.splice(index, 1);
-    setServiceList(list);
-  }
-
-  const handleQueryChange = (e, index) => {
-    const { name, value } = e.target
-    const list = [...serviceList];
-    list[index][name] = value;
-    setServiceList(list)
-  }
-
-
-
   const [platform, setPlatform] = useState('Online News Archive');
 
   const handleChangePlatform = (event) => {
     setPlatform(event.target.value);
   };
 
-  const [any, setAny] = useState("Any");
+  const [any, setAny] = useState("AND");
 
   const handleChangeAnyAll = (event) => {
     setAny(event.target.value);
   };
 
 
-  function createQuery() {
-    let query = "";
-    let anyAll = "";
+  // function createQuery() {
+  //   let query = "";
+  //   let anyAll = "";
 
-    if (any == "Any") {
-      anyAll = "OR"
-    } else {
-      anyAll = "AND"
-    }
+  //   if (any == "Any") {
+  //     anyAll = "OR"
+  //   } else {
+  //     anyAll = "AND"
+  //   }
 
-    for (let i = 0; i < serviceList.length; i++) {
-      if (i == serviceList.length - 1) {
-        query += serviceList[i].service
-      } else {
-        query += serviceList[i].service + " " + anyAll + " "
-      }
-    }
-    return query;
-  }
+  //   for (let i = 0; i < serviceList.length; i++) {
+  //     if (i == serviceList.length - 1) {
+  //       query += serviceList[i].service
+  //     } else {
+  //       query += serviceList[i].service + " " + anyAll + " "
+  //     }
+  //   }
+  //   return query;
+  // }
 
-  createQuery()
+  // createQuery()
   return (
-    <>
 
-      <div className="searchTitle">
-        <h1>Featured Collections</h1>
-      </div>
+    <div>
+       {/* regular list */}
+      <List props={any} />
 
-      <div>
-        <h1>Simple Search</h1>
+      {/*  negation list */}
+      <List props="AND NOT" />
+    </div>
 
-        {/* Choose Platform  */}
-        <div>
-          <Select
-            value={platform}
-            onChange={handleChangePlatform}
-          >
-            <MenuItem value={"Online News Archive"}>Online News Archive</MenuItem>
-            <MenuItem value={"Reddit"}>Reddit</MenuItem>
-            <MenuItem value={"Twitter"}>Twitter</MenuItem>
-            <MenuItem value={"Youtube"}>Youtube</MenuItem>
-          </Select>
-        </div>
-
-
-        {/* Choose Query Type */}
-        <div>
-          <Select
-            value={any}
-            onChange={handleChangeAnyAll}
-          >
-            <MenuItem value={"Any"}>Any</MenuItem>
-            <MenuItem value={"All"}>All</MenuItem>
-          </Select>
-        </div>
-
-
-
-        <div>
-
-          {serviceList.map((singleService, index) => (
-            <div key={index} className='services'>
-
-              <div className="firstDivision">
-                <input name="service" type="text" id="service" required
-                  value={singleService.service}
-                  onChange={(e) => handleQueryChange(e, index)} />
-
-                {serviceList.length - 1 === index && serviceList.length < 7 && (
-                  <button
-                    onClick={handleServiceAdd}
-                    type="button"
-                    className='add-btn'>
-                    <span>Add a Service</span>
-                  </button>
-                )}
-              </div>
-
-              <div className="second-division">
-                {serviceList.length > 1 &&
-                  <button onClick={() => handleServiceRemove(index)} type="button" className='remove-btn'>
-                    <span>Remove</span>
-                  </button>
-                }
-              </div>
-            </div>
-          ))}
-        </div>
-        
-        <h1> {createQuery()}</h1>
-      </div>
-
-    </>
   );
 }
