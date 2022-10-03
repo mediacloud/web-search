f01e6b482bf9998d9a9ee5225e9d48c4b7858028
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-03 18:12:51 -0400

    setQueryList, setNegatedList, setFromDate, setToDate

--- mcweb/frontend/src/features/search/searchSlice.js
+++ mcweb/frontend/src/features/search/searchSlice.js
@@ -2,16 +2,36 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const slice = createSlice({
   name: 'search',
-  initialState: { search: null},
+  initialState: { search: null, queryList: null, negatedQueryList: null, fromDate: null, toDate: null },
   reducers: {
     setSearch: (state, { payload }) => {
       state.search = payload.count;
+    },
+    setQueryList: (state, {payload}) => {
+      state.queryList = payload.query;
+    }, 
+    setNegatedQueryList: (state, {payload}) => {
+      state.negatedQueryList = payload.query;
+    }, 
+    setFromDate: (state, { payload }) => {
+      state.fromDate = payload;
+    },
+    setToDate: (state, { payload }) => {
+      state.toDate = payload;
     }
   },
 });
 
 export const setSearch = slice.actions.setSearch
+export const setQueryList = slice.actions.setQueryList
+export const setNegatedQueryList = slice.actions.setNegatedQueryList
+export const setFromDate = slice.actions.setFromDate
+export const setToDate = slice.actions.setToDate
 
 export const selectTotalAttention = (state) => state.search.search;
+export const selectQuery = (state) => state.search.queryList;
+export const selectNegatedQuery = (state) => state.search.negatedQueryList;
+export const selectFromDate = (state) => state.search.fromDate;
+export const selectToDate = (state) => state.search.toDate;
 
 export default slice.reducer;
