3913c07a24311f22ec12869f5935cad1c8ac763e
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2022-10-14 12:04:31 -0400

    testing

---
+++ mcweb/frontend/src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className="footer">
+        <a>Media Cloud is a consortium research project across multiple institutions, including the </a> 
+        <a href="https://www.umass.edu/">University of Massachusetts Amherst</a>
+        <a>,</a>
+        <a href="https://www.northeastern.edu/">Northeastern University</a>
+        <a>, and the </a>
+        <a href="https://cyber.law.harvard.edu/">Berkman Klein Center for Internet & Society at Harvard University</a>
+       <br/> 
+       <p> v0.1.0 </p>
+    </div>
+);
+
+export default Footer;  
