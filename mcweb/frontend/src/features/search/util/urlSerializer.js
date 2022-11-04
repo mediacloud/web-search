5854fee8dc20e97d9c81998d656875eeda09b6cb
Evan Leon <evan.leon.j@gmail.com>
2022-11-04 15:12:06 -0400

    Serialization support for hashtags

--- mcweb/frontend/src/features/search/util/urlSerializer.js
+++ mcweb/frontend/src/features/search/util/urlSerializer.js
@@ -6,22 +6,26 @@ const formatCollections = (collectionsArray) => collectionsArray.map((collection
   `${collection.id}>${collection.name}`
 ));
 
-// const hashReplace = (w) => {
-//   const hashPos = w.indexOf('#');
-//   if (hashPos !== -1) {
-//     w[hashPos] = '^';
-//   }
-//   console.log(w);
-//   return w;
-// };
+const setCharAt = (str, index, chr) => {
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
+};
+
+const hashReplace = (w) => {
+  const hashPos = w.indexOf('#');
+  if (hashPos !== -1) {
+    w = setCharAt(w, hashPos, '^');
+  }
+  return w;
+};
 
 const queryListHelper = (queryList) => {
   if (queryList[0].length < 1) return '';
-  const filtered = queryList.filter((queryWord) => queryWord.length >= 1);
-  // filtered = filtered.map(hashReplace);
-  // console.log(filtered);
+  let filtered = queryList.filter((queryWord) => queryWord.length >= 1);
+  filtered = filtered.map(hashReplace);
   return filtered.join(',');
 };
+
 const urlSerializer = (queryObject) => {
   const {
     queryList,
@@ -33,7 +37,6 @@ const urlSerializer = (queryObject) => {
     anyAll,
   } = queryObject;
   const query = queryListHelper(queryList);
-  // console.log(query);
   const negatedQuery = queryListHelper(negatedQueryList);
   const start = dayjs(startDate).format('MM-DD-YYYY');
   const end = dayjs(endDate).format('MM-DD-YYYY');
