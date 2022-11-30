f5ecc473e06762158725d535d2536a47170f6785
Evan Leon <78226696+Evan-Leon@users.noreply.github.com>
2022-11-30 09:08:15 -0500

    Update Search.jsx

--- mcweb/frontend/src/features/search/Search.jsx
+++ mcweb/frontend/src/features/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useEFfect} from 'react';
+import React, {useEffect} from 'react';
 import Button from '@mui/material/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSnackbar } from 'notistack';
