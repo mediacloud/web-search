b85a337f26c13591e18f750f4bf3781a0b1261fc
Rahul Bhargava <rahulbot@gmail.com>
2022-12-01 11:02:42 -0500

    show useful Twitter limit error #137

--- mcweb/backend/users/exceptions.py
+++ mcweb/backend/users/exceptions.py
@@ -1,4 +1,10 @@
+from mcweb.backend.search.providers import PLATFORM_TWITTER
+
 
 class OverQuotaException(Exception):
-    def __init__(self, provider, limit):
-        super().__init__("You have used up your quota for {} - {} hits per week".format(provider, limit))
+    def __init__(self, provider: str, limit: int):
+        if provider.startswith(PLATFORM_TWITTER):
+            super().__init__("You're limited to {} quieries per week against Twitter's API. We're still working on "
+                             "a \"bring your own API key\" feauture.".format(limit))
+        else:
+            super().__init__("You have used up your quota for {} - {} hits per week".format(provider, limit))
