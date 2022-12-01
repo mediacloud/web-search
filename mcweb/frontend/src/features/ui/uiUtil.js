925e57977debb2370308186df9fb95452e9fb5ad
Rahul Bhargava <rahulbot@gmail.com>
2022-12-01 11:33:45 -0500

    add admin link to header menu for staff

--- mcweb/frontend/src/features/ui/uiUtil.js
+++ mcweb/frontend/src/features/ui/uiUtil.js
@@ -1,9 +1,4 @@
-
-// to save us typing redundant info when using MUI <Menu>s
-export const defaultMenuOriginProps = {
-  anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
-  transformOrigin: { vertical: 'top', horizontal: 'right' },
-};
-
 // included so we can later move assets to a CDN if needed
 export const assetUrl = (assetPath) => `/static/${assetPath}`;
+
+export default assetUrl;
