61a669ecc00a770c95b140b5d112484dee4c8b7a
Evan Leon <evan.leon.j@gmail.com>
2022-12-05 14:30:41 -0500

    Rearrange start script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+curl -SL https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -o lid.176.bin
+mv lid.176.bin mcweb/backend/search/providers/language/
 python mcweb/manage.py migrate
 npm run build
 python mcweb/manage.py collectstatic --noinput
 
-curl -SL https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -o lid.176.bin
-mv lid.176.bin mcweb/backend/search/providers/language/
+
