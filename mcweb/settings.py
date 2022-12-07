4eb17a8a7ccca5dfbf3841151927cf654513e2e3
Rahul Bhargava <rahulbot@gmail.com>
2022-12-07 10:32:39 -0500

    version bump for relase

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -23,7 +23,7 @@ from django.core.exceptions import ImproperlyConfigured
 logger = logging.getLogger(__file__)
 
 # The static version of the app
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
