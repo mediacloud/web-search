07e2154a943a4f97be8bf048e4cc10a7407c79a2
Rahul Bhargava <rahulbot@gmail.com>
2022-12-08 22:05:40 -0500

    don't try to send emails if email not enabled

--- mcweb/util/send_emails.py
+++ mcweb/util/send_emails.py
@@ -1,6 +1,10 @@
 import threading
+import logging
 from django.core.mail import send_mail, EmailMessage
 from django.template.loader import render_to_string
+from mcweb.settings import EMAIL_HOST
+
+logger = logging.getLogger(__name__)
 
 
 # adapted with help from https://github.com/CryceTruly/django-tutorial-youtube/tree/main/templates Cryce Truly
@@ -15,15 +19,19 @@ class EmailThread(threading.Thread):
 
 
 def send_email(mail_params):
+    if not EMAIL_HOST:
+        return
     subject, body, from_email, recepient = mail_params
     print(mail_params)
     try:
         send_mail(subject, body, from_email, [recepient], fail_silently=False)
     except Exception as e: 
-        print(e)
+        logger.exception(e)
 
 
 def send_signup_email(user, request):
+    if not EMAIL_HOST:
+        return
     email_body = render_to_string('authentication/activate.html', {
         'user': user,
     })
@@ -38,4 +46,6 @@ def send_signup_email(user, request):
 
 
 def send_source_upload_email(title: str, text: str, to: str):
+    if not EMAIL_HOST:
+        return
     send_mail(title, text, 'system@mediacloud.org', [to], fail_silently=False)
