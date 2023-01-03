f0fba8bed9a0e3ca7d22d6ba341985ebed173494
Evan Leon <evan.leon.j@gmail.com>
2023-01-03 12:00:42 -0500

    Fix only able to choose one preview collection

--- mcweb/frontend/src/features/search/query/media-picker/CollectionSelectionTable.jsx
+++ mcweb/frontend/src/features/search/query/media-picker/CollectionSelectionTable.jsx
@@ -24,15 +24,15 @@ export default function CollectionSelectionTable({
           <tr key={c.id}>
             <td><Link target="_blank" rel="noopener noreferrer" to={`/collections/${c.id}`}>{c.name}</Link></td>
             <td>{c.notes}</td>
-            <td class="numeric">{asNumber(c.source_count)}</td>
+            <td className="numeric">{asNumber(c.source_count)}</td>
             <td>
               {!(alreadySelected(c.id)) && (
-                <IconButton size="sm" aria-label="add" onClick={() => dispatch(onAdd([{...c, type:'collection'}]))}>
+                <IconButton size="sm" aria-label="add" onClick={() => dispatch(onAdd([{ ...c, type: 'collection' }]))}>
                   <AddCircleIcon sx={{ color: '#d24527' }} />
                 </IconButton>
               )}
               {(alreadySelected(c.id)) && (
-                <IconButton size="sm" aria-label="remove" onClick={() => dispatch(onRemove([{...c, type:'collection'}]))}>
+                <IconButton size="sm" aria-label="remove" onClick={() => dispatch(onRemove({ ...c, type: 'collection' }))}>
                   <RemoveCircleIcon sx={{ color: '#d24527' }} />
                 </IconButton>
               )}
