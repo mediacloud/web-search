583d1e87c7d2b9c4d592074d1fabf834214efd0b
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-05 14:31:38 -0500

    deleted url's regarding the old change password

--- mcweb/backend/users/urls.py
+++ mcweb/backend/users/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     path('logout', views.logout),
     path('register', views.register),
     path('profile', views.profile),
-    path('send-email', views.reset_password_request),
-    path('email-exists', views.email_exists),
-    path('reset-password', views.reset_password),
     path('delete-user', views.delete_user)
 ]
