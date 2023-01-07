d7393bee41216f90d5fa274b63b821b1a4d67646
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-06 22:54:10 -0500

    link with props

--- mcweb/frontend/src/features/search/query/Link.js
+++ mcweb/frontend/src/features/search/query/Link.js
@@ -1,10 +1,70 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { setStartDate, setEndDate } from './querySlice';
+import dayjs from 'dayjs';
+import { useNavigate } from 'react-router-dom';
+import urlSerializer from '../util/urlSerializer';
+import { searchApi } from '../../../app/services/searchApi';
+import { setSearchTime } from './querySlice';
+import { latestAllowedEndDate } from '../util/platforms';
+
 
 export default function Link( {amountOfTime, typeOfTime}) {
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  const {
+    queryString,
+    queryList,
+    negatedQueryList,
+    startDate,
+    endDate,
+    collections,
+    sources,
+    platform,
+    anyAll,
+    advanced,
+  } = useSelector((state) => state.query);
+
+  const queryObject = {
+    queryList,
+    negatedQueryList,
+    queryString,
+    startDate,
+    endDate,
+    platform,
+    collections,
+    sources,
+    anyAll,
+    advanced,
+  };
 
   return (
-    <h1>Hello</h1>
+    <Link
+      underline="hover"
+      component="button"
+      variant="body2"
+      sx={{ marginRight: 3 }}
+
+      onClick={() => {
+        const day = dayjs(endDate, "MM-DD-YYYY").subtract(1, 'month').format('MM/DD/YYYY');
+
+        dispatch(setStartDate(day));
+
+        navigate(
+          `/search${urlSerializer(queryObject)}`,
+          { options: { replace: true } },
+        );
+        dispatch(searchApi.util.resetApiState());
+        dispatch(setSearchTime(dayjs().unix()));
+
+
+      }}
+    >
+      Last Month
+    </Link>
   )
 
 }
