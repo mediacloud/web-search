696fdd95ded3c43303e8e3b495770f0c5ef0ee49
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-06 22:53:52 -0500

    refactored

--- mcweb/frontend/src/features/search/query/DefaultDates.jsx
+++ mcweb/frontend/src/features/search/query/DefaultDates.jsx
@@ -1,54 +1,19 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setStartDate, setEndDate } from './querySlice';
-import dayjs from 'dayjs';
-import { useNavigate } from 'react-router-dom';
-import urlSerializer from '../util/urlSerializer';
-import { searchApi } from '../../../app/services/searchApi';
-import { setSearchTime } from './querySlice';
 
-import { latestAllowedEndDate } from '../util/platforms';
-import { useSnackbar } from 'notistack';
-
-import { Link } from '@mui/material';
-
+import Link from './Link';
 
 export default function DefaultDates() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-
-  const {
-    queryString,
-    queryList,
-    negatedQueryList,
-    startDate,
-    endDate,
-    collections,
-    sources,
-    platform,
-    anyAll,
-    advanced,
-  } = useSelector((state) => state.query);
-
-  const queryObject = {
-    queryList,
-    negatedQueryList,
-    queryString,
-    startDate,
-    endDate,
-    platform,
-    collections,
-    sources,
-    anyAll,
-    advanced,
-  };
   
   return (
     <>
 
-    <h1>Hello</h1>
+      <Link amountOfTime="1" typeOfTime="month" />
 
+      <Link amountOfTime="3" typeOfTime="month" />
+
+      <Link amountOfTime="1" typeOfTime="year" />
+    
     </>
   );
 }
