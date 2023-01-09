81479e9fe5c937eb2724170d3b7830fa919eef5f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-09 10:55:20 -0500

    remove searching

--- mcweb/frontend/src/features/search/query/DefaultDates.js
+++ mcweb/frontend/src/features/search/query/DefaultDates.js
@@ -1,46 +1,20 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { setStartDate } from './querySlice';
+import { setStartDate, setEndDate } from './querySlice';
 import dayjs from 'dayjs';
 import { useNavigate } from 'react-router-dom';
-import urlSerializer from '../util/urlSerializer';
-import { searchApi } from '../../../app/services/searchApi';
-import { setSearchTime } from './querySlice';
-
 
 import { Link } from "@mui/material";
 
 export default function DefaultDates({ amountOfTime, typeOfTime, message }) {
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
 
   const {
-    queryString,
-    queryList,
-    negatedQueryList,
-    startDate,
     endDate,
-    collections,
-    sources,
-    platform,
-    anyAll,
-    advanced,
   } = useSelector((state) => state.query);
 
-  const queryObject = {
-    queryList,
-    negatedQueryList,
-    queryString,
-    startDate,
-    endDate,
-    platform,
-    collections,
-    sources,
-    anyAll,
-    advanced,
-  };
 
   return (
 
@@ -55,13 +29,6 @@ export default function DefaultDates({ amountOfTime, typeOfTime, message }) {
         const day = dayjs(endDate, "MM-DD-YYYY").subtract(amountOfTime, typeOfTime).format('MM/DD/YYYY');
 
         dispatch(setStartDate(day));
-
-        navigate(
-          `/search${urlSerializer(queryObject)}`,
-          { options: { replace: true } },
-        );
-        dispatch(searchApi.util.resetApiState());
-        dispatch(setSearchTime(dayjs().unix()));
 
       }}
     >
