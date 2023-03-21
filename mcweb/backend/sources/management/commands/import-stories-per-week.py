9de6030780d28efed521bb62ad76907ec8ac2aef
rutvi21 <rutvi.t@ahduni.edu.in>
2023-03-21 09:31:14 -0400

    alert system initial commit

--- mcweb/backend/sources/management/commands/import-stories-per-week.py
+++ mcweb/backend/sources/management/commands/import-stories-per-week.py
@@ -3,6 +3,8 @@ import os
 from subprocess import call
 import csv
 import logging
+import pandas as pd
+import numpy as np
 from django.core.exceptions import ObjectDoesNotExist
 from ...models import Source
 
@@ -29,12 +31,25 @@ class Command(BaseCommand):
 
         with open(file_path) as f:
             csv_reader = csv.DictReader(f)
-            for row in csv_reader:
-                try:
-                    source = Source.objects.get(id=int(row['sources_id']))
-                    source.stories_per_week = round((int(float(row['count'])) / 4))
-                    source.save()
-                except ObjectDoesNotExist as e:
-                    logger.error("Unknown source id {}".format(row['media_id']))
+            data = pd.DataFrame(csv_reader)
+            data['mean'] = data.groupby('source_id')['count'].rolling(30).mean().reset_index(0, drop=True)
+            #  standard deviation for each source
+            data['std_dev'] = data.groupby('source_id')['count'].rolling(30).std().reset_index(0, drop=True)
+            # Flag sources that have an average of more than 5 stories per day
+            data['flag'] = np.where(data['mean'] > 5, 1, 0)
+            # Identify days where a source's number of stories is more than 2 standard deviations below the 30-day average
+            data['alert'] = np.where(((data['count'] < (data['mean'] - 2*data['std_dev'])) & (data['flag'] == 1)), 1, 0)
+
+            for index, row in data.iterrows():
+                if row['alert'] == 1:
+                    print(row['date'], row['source_id'])
+            # for row in csv_reader:
+                # try:
+                #     source = Source.objects.get(id=int(row['sources_id']))
+                #     source.stories_per_week = round((int(float(row['count'])) / 4))
+                #     source.save()
+                # except ObjectDoesNotExist as e:
+                #     logger.error("Unknown source id {}".format(row['media_id']))
+
 
         self.stdout.write(self.style.SUCCESS('Done from "%s"' % file_path))
