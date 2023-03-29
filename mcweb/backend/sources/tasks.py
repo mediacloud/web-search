6a69ed9362a6f6ec46c985669a9e348fa17b948e
Evan-Leon <evan.leon.j@gmail.com>
2023-03-29 15:20:55 -0400

    Move alert_system and make it a task, change management name for alert-system, run task from management, add json of collections to monitor

--- mcweb/backend/sources/tasks.py
+++ mcweb/backend/sources/tasks.py
@@ -6,6 +6,10 @@ Background tasks for "sources" app
 # standard:
 import datetime as dt
 import logging
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+import logging
+
 
 # PyPI:
 from background_task import background
@@ -14,8 +18,19 @@ from feed_seeker import generate_feed_urls
 from mcmetadata.feeds import normalize_url
 from django.core.management import call_command
 from django.utils import timezone
+import pandas as pd
+import numpy as np
+
 # from sources app:
-from .models import Feed, Source
+from .models import Feed, Source, Collection
+
+# emails
+from util.send_emails import send_alert_email
+
+
+#rss fetcher
+from .rss_fetcher_api import RssFetcherApi
+
 
 SCRAPE_TIMEOUT_SECONDS = 120
 
@@ -76,13 +91,58 @@ days_until_friday = (4 - today.weekday()) % 7
 next_friday = today + dt.timedelta(days=days_until_friday)
 run_datetime = dt.datetime.combine(next_friday, run_at)
 
+
+
+def run_alert_system():
+    with open('mcweb/backend/sources/data/collections-to-monitor.json') as collection_ids:
+        collection_ids = collection_ids.read()
+        collection_ids = json.loads(collection_ids)
+
+    task = _alert_system(collection_ids,
+                        verbose_name=f"source alert system {dt.datetime.now()}",
+                        remove_existing_tasks=True)
+    return {'task': _return_task(task)}
+
 @background(schedule=run_datetime, queue='weekly-import')
-def import_stories_per_week():
-    try:
-        print("xyz")
-        call_command('import-stories-per-week')
-    except Exception as e:
-        print(f"Failed to run import-stories-per-week: {e}")
+def _alert_system(collection_ids):
+        sources = set()
+        for collection_id in collection_ids:
+            try:
+                collection = Collection.objects.get(pk=collection_id)
+                source_relations = set(collection.source_set.all())
+                # print(source_relations)
+                sources = sources | source_relations
+            except:
+                print(collection_id)
+
+        with RssFetcherApi() as rss:
+        # stories_by_source = rss.stories_by_source() # This will generate tuples with (source_id and stories_per_day)
+          
+            email=""
+            alert_count = 0
+            for source in sources:
+                print(source)
+                stories_fetched = rss.source_stories_fetched_by_day(source.id) 
+                # print(stories_fetched)
+                counts = [d['count'] for d in stories_fetched]  # extract the count values
+                mean = np.mean(counts) 
+                std_dev = np.std(counts)  
+                # todays_count = counts[-1]
+
+                # stories_published = rss.source_stories_published_by_day(source.id)
+                # counts_published = [d['count'] for d in stories_published]  
+                # mean_published = np.mean(counts_published)  
+                # std_dev_published = np.std(counts_published)  
+
+                if (std_dev * 2) < mean:
+                    email += f"Source {source.id}: {source.name} has a story/day average of {mean} over the last 30 days, which is more than two standard deviations (standard_deviation: {std_dev}) above the mean \n"
+                    alert_count += 1
+                    print(email)
+            
+            if(email):
+                email += f"total alert count = {alert_count} \n"
+                send_alert_email(email)
+
 
 def _return_task(task):
     """
