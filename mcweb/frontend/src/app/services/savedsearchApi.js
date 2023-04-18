17b8f4a16c977b34236f515e44e778d960e88ed7
elissa-alarmani <91026581+elissa-alarmani@users.noreply.github.com>
2023-04-18 14:38:23 -0400

    savedsearch api created and updated

---
+++ mcweb/frontend/src/app/services/savedsearchApi.js
@@ -0,0 +1,69 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const savedsearchApi = createApi({
+  reducerPath: 'savedsearchApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/search/',
+    prepareHeaders: (headers) => {
+      // Django requires this for security (cross-site forgery protection) once logged in
+      headers.set('X-Csrftoken', window.CSRF_TOKEN);
+      return headers;
+    },
+  }),
+  tagTypes: ['SavedSearch'],
+
+  endpoints: (builder) => ({
+    createSavedSearch: builder.mutation({
+      query: (savedsearch) => ({
+        url: 'savedsearch/',
+        method: 'POST',
+        body: { savedsearch },
+      }),
+      invalidatesTags: ['SavedSearch'],
+    }),
+    getSavedSearch: builder.query({
+      query: (id) => ({
+        url: `savedsearch/${id}/`,
+        method: 'GET',
+      }),
+      providesTags: (result, id) => (result
+        ? [{ type: 'SavedSearch', id }]
+        : ['SavedSearch']),
+    }),
+    listSavedSearches: builder.query({
+      query: () => ({
+        url: 'savedsearch/',
+        method: 'GET',
+      }),
+      providesTags: (result, id) => (result
+        ? [{ type: 'SavedSearch', id }]
+        : ['SavedSearch']),
+    }),
+    updateSavedSearch: builder.mutation({
+      query: (savedsearch) => ({
+        url: `savedsearch/${savedsearch.id}`,
+        method: 'PATCH',
+        body: { ...savedsearch },
+      }),
+      invalidatesTags: ['SavedSearch'],
+    }),
+    deleteSavedSearch: builder.mutation({
+      query: (savedsearch) => ({
+        url: `savedsearch/${savedsearch.id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['SavedSearch'],
+    }),
+  }),
+});
+
+// search/attentionOverTime
+// search
+// action: get back Json. Save it to searchResults
+export const {
+  useCreateSavedSearchMutation,
+  useGetSavedSearchQuery,
+  useListSavedSearchesQuery,
+  useUpdateSavedSearchMutation,
+  useDeleteSavedSearchMutation,
+} = savedsearchApi;
