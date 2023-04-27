fe3bd3ffae7908bc58f12955aa9117ed34961e52
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-27 14:02:53 -0400

    holds the function for date validation; dealing with eslint issues

--- mcweb/frontend/src/features/search/util/platforms.js
+++ mcweb/frontend/src/features/search/util/platforms.js
@@ -58,3 +58,22 @@ export const defaultPlatformQuery = (platform) => {
   if (platform === PLATFORM_TWITTER) return ['🙂'];
   if (platform === PLATFORM_YOUTUBE) return ['🙂'];
 };
+
+export const validateDate = (currentDate, minDate, maxDate) => {
+  const daysBetweenMinAndtoDate = dayjs(currentDate).diff(dayjs(minDate), 'day');
+
+  const daysBetweenToDateAndMax = dayjs(maxDate).diff(dayjs(currentDate), 'day');
+
+  const daysBetweenMinAndMax = dayjs(maxDate).diff(dayjs(minDate), 'day');
+
+  const currentYear = dayjs(currentDate).year();
+  const minYear = dayjs(minDate).year();
+  const maxYear = dayjs(maxDate).year();
+
+  const isCurrentYearInBetweenMinAndMax = currentYear >= minYear && currentYear <= maxYear;
+
+  const isCurrentDateInBetween = daysBetweenMinAndtoDate <= daysBetweenMinAndMax
+    && daysBetweenToDateAndMax <= daysBetweenMinAndMax;
+
+  return isCurrentYearInBetweenMinAndMax && isCurrentDateInBetween;
+};
