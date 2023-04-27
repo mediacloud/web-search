4d76e4018e4c8838f72fc3a30cb81296c91e34c6
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-27 16:47:37 -0400

    testing

--- mcweb/frontend/__tests__/testEarlestAllowedStartDate.js
+++ mcweb/frontend/__tests__/testEarlestAllowedStartDate.js
@@ -10,36 +10,36 @@ test('PROVIDER_NEWS_WAYBACK_MACHINE', () => {
 });
 
 test('PROVIDER_NEWS_MEDIA_CLOUD', () => {
-  const today = dayjs();
+  const result = dayjs('2010-01-01').format('MM/DD/YYYY');
   const provider = 'onlinenews-mediacloud';
 
-  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(today.subtract('1', 'day').format('MM/DD/YYYY'));
+  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(result);
 });
 
-// test('PROVIDER_REDDIT_PUSHSHIFT', () => {
-//   const today = dayjs().format('MM/DD/YYYY');
-//   const provider = 'reddit-pushsift';
+test('PROVIDER_REDDIT_PUSHSHIFT', () => {
+  const result = dayjs('2022-11-1').format('MM/DD/YYYY');
+  const provider = 'reddit-pushshift';
 
-//   expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(today);
-// });
+  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(result);
+});
 
-// test('PROVIDER_YOUTUBE_YOUTUBE', () => {
-//   const today = dayjs().format('MM/DD/YYYY');
-//   const provider = 'youtube-youtube';
+test('PROVIDER_YOUTUBE_YOUTUBE', () => {
+  const result = dayjs('2010-01-01').format('MM/DD/YYYY');
+  const provider = 'youtube-youtube';
 
-//   expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(today);
-// });
+  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(result);
+});
 
-// test('empty input', () => {
-//   const today = dayjs().format('MM/DD/YYYY');
-//   const provider = '';
+test('empty input', () => {
+  const result = dayjs('2010-01-01').format('MM/DD/YYYY');
+  const provider = '';
 
-//   expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(today);
-// });
+  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(result);
+});
 
-// test('null input', () => {
-//   const today = dayjs().format('MM/DD/YYYY');
-//   const provider = null;
+test('null input', () => {
+  const result = dayjs('2010-01-01').format('MM/DD/YYYY');
+  const provider = null;
 
-//   expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(today);
-// });
+  expect(earliestAllowedStartDate(provider).format('MM/DD/YYYY')).toBe(result);
+});
