76835eb2a68806a8f1b2a75a21bf912f3fea36e1
elissa-alarmani <elissa1327@gmail.com>
2023-05-02 12:22:29 -0400

    only displaying geo collections if platform is onlinenews

--- mcweb/frontend/src/features/search/query/media-picker/GeographicCollectionsPicker.jsx
+++ mcweb/frontend/src/features/search/query/media-picker/GeographicCollectionsPicker.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { addPreviewSelectedMedia, removePreviewSelectedMedia } from '../querySlice';
 import { useGetGlobalCollectionsQuery } from '../../../../app/services/collectionsApi';
 import MediaPickerSelectionTable from './MediaPickerSelectionTable';
@@ -7,22 +8,25 @@ import MediaPickerSelectionTable from './MediaPickerSelectionTable';
 export default function GeographicCollectionsPicker({ platform, queryIndex }) {
   const { data, isLoading } = useGetGlobalCollectionsQuery({ platform });
 
+  const { previewCollections } = useSelector((state) => state.query[queryIndex]);
+
   if (isLoading) {
     return (<div>Loading...</div>);
   }
-
-  const formatedData = data.countries.map((countryAndCollections) => countryAndCollections.collections.map((collection) => ({
-    id: collection.tags_id,
-    name: collection.label,
-    notes: collection.description,
-    source_count: collection.length,
-  }))).flat();
+  const formattedData = platform === 'onlinenews'
+    ? data.countries.map((countryAndCollections) => countryAndCollections.collections.map((collection) => ({
+      id: collection.tags_id,
+      name: collection.label,
+      notes: collection.description,
+      source_count: collection.length,
+    }))).flat()
+    : [];
 
   return (
     <div className="container">
       <MediaPickerSelectionTable
-        selected={[]}
-        matching={formatedData}
+        selected={previewCollections}
+        matching={formattedData}
         onAdd={addPreviewSelectedMedia}
         onRemove={removePreviewSelectedMedia}
         collection
