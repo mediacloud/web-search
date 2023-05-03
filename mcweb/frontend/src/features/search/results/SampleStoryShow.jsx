7e190825ca46641a71e77df593c2ef46bd58cfc8
elissa-alarmani <elissa1327@gmail.com>
2023-05-03 15:14:55 -0400

    added props to samplestoryshow, removed anchorEl bug

--- mcweb/frontend/src/features/search/results/SampleStoryShow.jsx
+++ mcweb/frontend/src/features/search/results/SampleStoryShow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
@@ -6,12 +6,21 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { PROVIDER_NEWS_WAYBACK_MACHINE } from '../util/platforms';
+import { PROVIDER_NEWS_WAYBACK_MACHINE, PROVIDER_NEWS_MEDIA_CLOUD } from '../util/platforms';
 import { googleFaviconUrl } from '../../ui/uiUtil';
+import Permissioned, { ROLE_ADMIN } from '../../auth/Permissioned';
 
 export default function SampleStoryShow({
-  data, lSTP, platform, handleClick, open, handleClose, anchorEl,
+  data, lSTP, platform,
 }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const getStoryId = (url) => {
     if (!url) return null;
     const parts = url.split('/');
@@ -79,6 +88,44 @@ export default function SampleStoryShow({
               </Menu>
             </td>
             )}
+            {([PROVIDER_NEWS_MEDIA_CLOUD].includes(platform)
+          && lSTP === PROVIDER_NEWS_MEDIA_CLOUD) && (
+
+            <td>
+              <Permissioned role={ROLE_ADMIN}>
+                <Button
+                  variant="outlined"
+                  onClick={handleClick}
+                  aria-controls={open ? 'basic-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                  endIcon={<KeyboardArrowDownIcon />}
+                >
+                  Info
+                </Button>
+              </Permissioned>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem>
+                  <a href={sampleStory.url} target="_blank" rel="noreferrer">
+                    visit original URL
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <Link
+                    to={`/story/${platform}/${getStoryId(sampleStory.url)}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    view extracted content
+                  </Link>
+                </MenuItem>
+              </Menu>
+            </td>
+            )}
           </tr>
         ))}
       </tbody>
@@ -90,7 +137,7 @@ SampleStoryShow.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({
     archived_url: PropTypes.string,
     article_url: PropTypes.string,
-    id: PropTypes.string,
+    id: PropTypes.number,
     language: PropTypes.string,
     media_name: PropTypes.string,
     media_url: PropTypes.string,
@@ -100,12 +147,4 @@ SampleStoryShow.propTypes = {
   })).isRequired,
   lSTP: PropTypes.string.isRequired,
   platform: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  handleClose: PropTypes.func.isRequired,
-  anchorEl: PropTypes.element,
-};
-
-SampleStoryShow.defaultProps = {
-  anchorEl: undefined,
 };
