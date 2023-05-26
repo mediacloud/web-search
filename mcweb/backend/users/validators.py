691cd5971797cdf93f704d8f4979b7958d497c45
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-26 15:23:44 -0400

    validators file for the minimum amount of numbers in the password and the amount of special characters

---
+++ mcweb/backend/users/validators.py
@@ -0,0 +1,53 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+
+class MinimumAmountOfNumbers:
+    def __init__(self, minimum_amount_of_numbers=3):
+        self.minimum_amount_of_numbers = minimum_amount_of_numbers
+
+    def validate(self, password, user=None):
+        count = 0
+        for char in password:
+            if char.isdigit():
+                count+=1
+
+        if count < self.minimum_amount_of_numbers:
+            raise ValidationError(
+                _("This password must contain at least %(minimum_amount_of_numbers)d numbers."),
+                code="requires_more_numbers",
+                params={"minimum_amount_of_numbers": self.minimum_amount_of_numbers},
+            )
+
+    def get_help_test(self):
+        return _(
+            "Your password must contain at least %(minimum_amount_of_numbers)d numbers."
+            % {"minimum_amount_of_numbers": self.minimum_amount_of_numbers},
+        )
+
+
+class SpecialCharacters:
+
+    def __init__(self, minimum_amount_of_special_characters=1):
+        self.minimum_amount_of_special_characters = minimum_amount_of_special_characters
+        self.special_characters = ['!', '@', '#', '$']
+
+    def validate(self, password, user=None):
+        
+        count = 0
+        for char in password: 
+            if char in self.special_characters: 
+                count+=1 
+
+        if count < self.minimum_amount_of_special_characters:
+            raise ValidationError(
+                _("This password must contain at least %(minimum_amount_of_special_characters)d special characters."),
+                code="requires_more_numbers",
+                params={"minimum_amount_of_special_characters": self.minimum_amount_of_special_characters},
+            )
+
+    def get_help_test(self):
+        return _(
+            "Your password must contain at least %(minimum_amount_of_special_characters)d special characters."
+            % {"minimum_amount_of_special_characters": self.minimum_amount_of_special_characters},
+        )
