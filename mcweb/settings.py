58b77568b291e9bc058a7ed1bdb8bc04b5dc86ec
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-26 15:24:17 -0400

    added custom validators

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -104,9 +104,24 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 10,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        "NAME": "backend.users.validators.MinimumAmountOfNumbers",
+    },
+     {
+        "NAME": "backend.users.validators.MinimumAmountOfSpecialCharacters",
+    },
 ]
 
 
