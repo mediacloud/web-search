cad6974e0591d8c462d49ccb53cf6df66be2925c
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-01 21:00:18 -0400

    added comments, removed streamer, created csv file, sent csv in an email with new send_large_download_csv_email function

--- mcweb/backend/search/tasks.py
+++ mcweb/backend/search/tasks.py
@@ -4,34 +4,34 @@ Background tasks for 'download_all_content_csv'
 
 from ..users.models import QuotaHistory
 from .utils import parse_query
-import datetime as dt
 import logging
 import collections
 import logging
 import time
-import backend.util.csv_stream as csv_stream
+from io import StringIO
+import csv
 from background_task import background
 from ..sources.tasks import _return_task
-from django.http import HttpResponse, HttpResponseBadRequest
-from util.send_emails import send_email
-import settings
-
+from util.send_emails import send_large_download_csv_email
 
 # emails
 from util.send_emails import send_alert_email
 
 import mc_providers as providers
+import csv
 
 
 logger = logging.getLogger(__name__)
 
 
-def download_all_large_content_csv(queryState, count, user_id, user_isStaff):
-    task = _download_all_large_content_csv(queryState, user_id, user_isStaff)
+def download_all_large_content_csv(queryState, user_id, user_isStaff):
     
+    # test email that the function is called
+    task = _download_all_large_content_csv(queryState, user_id, user_isStaff)
+
     return {'task': _return_task(task)}
 
-@background()
+@background(remove_existing_tasks=True)
 def _download_all_large_content_csv(queryState, user_id, user_isStaff):
     data = []
     for query in queryState:
@@ -40,27 +40,30 @@ def _download_all_large_content_csv(queryState, user_id, user_isStaff):
         provider = providers.provider_by_name(provider_name)
         data.append(provider.all_items(query_str, start_date, end_date, **provider_props))
     
+    # iterator function
     def data_generator():
         for result in data:
             first_page = True
             for page in result:
-                QuotaHistory.increment(
-                    user_id, user_isStaff, provider_name)
+                QuotaHistory.increment(user_id, user_isStaff, provider_name)
                 if first_page:  # send back column names, which differ by platform
                     yield sorted(list(page[0].keys()))
                 for story in page:
-                    ordered_story = collections.OrderedDict(
-                        sorted(story.items()))
+                    ordered_story = collections.OrderedDict(sorted(story.items()))
                     yield [v for k, v in ordered_story.items()]
                 first_page = False
 
+
+    # code from: https://stackoverflow.com/questions/17584550/attach-generated-csv-file-to-email-and-send-with-django
+    csvfile = StringIO()
+    csvwriter = csv.writer(csvfile)
     filename = "mc-{}-{}-content.csv".format(provider_name, _filename_timestamp())
-    streamer = csv_stream.CSVStream(filename, data_generator)
+   
+    for data in data_generator():
+        csvwriter.writerow(data)
 
-    mail_params = ('test', 'testing', 'system@mediacloud.org','evanjacobsuslovich@gmail.com')
-    send_email(mail_params)
+    logger.info("Sent CSV Email")
+    send_large_download_csv_email(filename, csvfile, 'evanjacobsuslovich@gmail.com')
     
-    
-
 def _filename_timestamp() -> str:
     return time.strftime("%Y%m%d%H%M%S", time.localtime())
