5a8da039241736883a65df9bdca4741fedbe4f7e
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-02 22:23:37 -0400

    added url

--- mcweb/backend/search/urls.py
+++ mcweb/backend/search/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('download-top-words-csv', views.download_words_csv),
     path('count-over-time', views.count_over_time),
     path('download-counts-over-time-csv', views.download_counts_over_time_csv),
-    path('download-all-content-csv', views.download_all_content_csv), # handling large downloads (TotalAttentionResults.jsx)
+    path('download-all-content-csv', views.download_all_content_csv),
     path('story', views.story_detail),
     path('languages', views.languages),
-    path('download-top-languages-csv', views.download_languages_csv)
+    path('download-top-languages-csv', views.download_languages_csv),
+    path('send-email-large-download-csv', views.send_email_large_download_csv)
 ]
