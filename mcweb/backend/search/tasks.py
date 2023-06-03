9f3466cf67f58a394c15e62ac3f6697f44804e0c
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-02 22:23:56 -0400

    uses email from frontend

--- mcweb/backend/search/tasks.py
+++ mcweb/backend/search/tasks.py
@@ -26,16 +26,15 @@ import csv
 logger = logging.getLogger(__name__)
 
 
-def download_all_large_content_csv(queryState, user_id, user_isStaff):
-    task = _download_all_large_content_csv(queryState, user_id, user_isStaff)
+def download_all_large_content_csv(queryState, user_id, user_isStaff, email):
+    task = _download_all_large_content_csv(queryState, user_id, user_isStaff, email)
     return {'task': _return_task(task)}
 
 @background(remove_existing_tasks=True)
-def _download_all_large_content_csv(queryState, user_id, user_isStaff):
+def _download_all_large_content_csv(queryState, user_id, user_isStaff, email):
     data = []
     for query in queryState:
-        start_date, end_date, query_str, provider_props, provider_name = parse_query(
-            query, 'GET')
+        start_date, end_date, query_str, provider_props, provider_name = parse_query(query, 'GET')
         provider = providers.provider_by_name(provider_name)
         data.append(provider.all_items(query_str, start_date, end_date, **provider_props))
     
@@ -61,11 +60,8 @@ def _download_all_large_content_csv(queryState, user_id, user_isStaff):
     for data in data_generator():
         csvwriter.writerow(data)
 
-    # compressed_file = BytesIO()
-    # with gz.
-    
     logger.info("Sent CSV Email")
-    send_large_download_csv_email(filename, csvfile, 'evanjacobsuslovich@gmail.com')
+    send_large_download_csv_email(filename, csvfile, email)
     
 def _filename_timestamp() -> str:
     return time.strftime("%Y%m%d%H%M%S", time.localtime())
