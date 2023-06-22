051fe6d78f61e41594a15628f0e93252e238f873
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-22 13:10:31 -0400

    duplicate ... put token inside

---
+++ mcweb/frontend/src/features/search/util/prepareQueries2.js
@@ -0,0 +1,39 @@
+import queryGenerator from './queryGenerator';
+
+export default function prepareQueries2(queryState, token) {
+  const queryArray = queryState.map((singleQuery) => {
+    const {
+      queryList,
+      queryString,
+      negatedQueryList,
+      platform,
+      startDate,
+      endDate,
+      collections,
+      sources,
+      anyAll,
+    } = singleQuery;
+
+    const fullQuery = () => {
+      let queryReturn = '';
+      if (queryString.length !== 0) {
+        queryReturn = queryString;
+      } else {
+        queryReturn = queryGenerator(queryList, negatedQueryList, platform, anyAll);
+      }
+      return queryReturn;
+    };
+
+    return {
+      query: fullQuery(),
+      startDate,
+      endDate,
+      collections,
+      sources,
+      platform,
+      token,
+    };
+  });
+
+  return queryArray;
+}
