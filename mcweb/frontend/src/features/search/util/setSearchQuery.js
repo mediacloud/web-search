c14ca602f4700e099993d7f3ef32e1cedd081f38
Evan-Leon <evan.leon.j@gmail.com>
2023-06-29 11:21:24 -0400

    Quick fix for negated query problem

--- mcweb/frontend/src/features/search/util/setSearchQuery.js
+++ mcweb/frontend/src/features/search/util/setSearchQuery.js
@@ -163,7 +163,7 @@ const setSearchQuery = (searchParams, dispatch) => {
 
   negatedQuery = negatedQuery ? negatedQuery.split(',') : null;
   negatedQuery = formatQuery(negatedQuery);
-  negatedQuery = negatedQuery ? sizeQuery(negatedQuery) : null;
+  negatedQuery = sizeQuery(negatedQuery);
 
   queryStrings = queryStrings ? handleDecode(queryStrings) : null; // come back to
 
