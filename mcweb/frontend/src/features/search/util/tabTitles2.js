5a4b474dff5562151f5db05e75ea073dfb2da35a
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-09 14:27:08 -0400

    clean up, include collectionTitle function

--- mcweb/frontend/src/features/search/util/tabTitles2.js
+++ mcweb/frontend/src/features/search/util/tabTitles2.js
@@ -2,63 +2,35 @@ import { PROVIDER_NEWS_MEDIA_CLOUD } from './platforms';
 import queryGenerator from './queryGenerator';
 import compareArrays from './compareArrays';
 import allDuplicates from './tabTitleHelpers/allDuplicates';
+import collectionTitle from './tabTitleHelpers/collectionTitle';
 
 const createTitle = (queryList, negatedQueryList, platform, anyAll, queryString) => {
-  if (queryString) {
-    return queryString;
-  }
+  if (queryString) return queryString;
   return queryGenerator(queryList, negatedQueryList, platform, anyAll);
-};
-
-const createCollectionTitle = (names) => {
-  let collectionTitle = '';
-  for (let i = 0; i < names.length; i += 1) {
-    if (names[i].length > 20) {
-      collectionTitle += `'${names[i].substring(0, 20)}' ... & `;
-    } else {
-      collectionTitle += `'${names[i]}'  & `;
-    }
-  }
-  return collectionTitle.slice(0, -2);
 };
 
 const tabTitle2 = (queryList, negatedQueryList, anyAll, queryString, index, queryState, collectionNames) => {
   if (queryState) {
-    const titles = queryState.map(
-      (query) => createTitle(query.queryList, query.negatedQueryList, PROVIDER_NEWS_MEDIA_CLOUD, query.anyAll, query.queryString),
-    );
-    if (queryState.length > 1) {
-      console.log(allDuplicates(null, titles));
-      if (allDuplicates(null, titles)) {
-        console.log(allDuplicates(compareArrays, collectionNames));
-        if (!allDuplicates(compareArrays, collectionNames)) {
-          return collectionNames[index];
-        }
+    // eslint-disable-next-line max-len
+    const titles = queryState.map((query) => createTitle(query.queryList, query.negatedQueryList, PROVIDER_NEWS_MEDIA_CLOUD, query.anyAll, query.queryString));
+    if (queryState.length === 1) return titles[index];
+
+    // titles are duplicates
+    if (allDuplicates(titles)) {
+      // collectionNames are duplicates
+      if (allDuplicates(collectionNames, compareArrays)) {
         return `Query ${index + 1} `;
       }
-
-      /*
-clever idea:
-
-the system heuristic be able to determine which part of the queries
-are different and use that for naming. Ie. if they search for
-'robot AND monkey' and 'robot AND dog', it should name the queries
-'monkey' and 'dog' based on the fact that they both start with "robot AND"
-*/
-
-      // IMPLEMENT here
+      // use collection names
+      return collectionTitle(collectionNames[index]);
     }
   }
 
   // queryState isn't passed in (setSearchQuery)
   const title = createTitle(queryList, negatedQueryList, PROVIDER_NEWS_MEDIA_CLOUD, anyAll, queryString);
 
-  if (title === '*') {
-    return `Query ${index + 1} `;
-  }
-  if (title.length > 20) {
-    return `${title.substring(0, 20)} ...`;
-  }
+  if (title === '*') return `Query ${index + 1} `;
+  if (title.length > 20) return `${title.substring(0, 20)} ...`;
   return title;
 };
 
