f52436a75fde15ce845cc927eaeeb094335ffaf1
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-10 14:16:28 -0400

    create collection titles

--- mcweb/frontend/src/features/search/util/tabTitleHelpers/collectionTitle.js
+++ mcweb/frontend/src/features/search/util/tabTitleHelpers/collectionTitle.js
@@ -1,5 +1,5 @@
 const collectionTitle = (collections) => {
-  if (collections.length === 0) return 'No Collections';
+  if (collections.length === 0) return 'No Selected Collections or Sources';
   const titles = collections.map((collection) => `'${collection}'`);
   return titles.join(' & ');
 };
