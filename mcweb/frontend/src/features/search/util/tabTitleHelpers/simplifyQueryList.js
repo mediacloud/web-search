dc1177fd8722e59f3ead7f401171cd8fd7ca91fe
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-11 09:51:25 -0400

    comments, remove [] from arrays, if all values are removed return the original array

--- mcweb/frontend/src/features/search/util/tabTitleHelpers/simplifyQueryList.js
+++ mcweb/frontend/src/features/search/util/tabTitleHelpers/simplifyQueryList.js
@@ -1,20 +1,39 @@
 import allDuplicates from './allDuplicates';
 import existsInAll from './existsInAll';
 import removePhrase from './removePhrase';
+import compareArrays from '../compareArrays';
 
 const simplifyQueryList = (index, queryLists, anyAlls) => {
-  const updatedQueryLists = queryLists;
-  if (allDuplicates(anyAlls)) {
-    let uniquePhrases = [...new Set(updatedQueryLists.flat())];
+  const originalQuery = queryLists[index];
+  let updatedQueryLists = queryLists;
 
+  // remove all '[]' from arrays
+  updatedQueryLists.forEach((query, i) => {
+    updatedQueryLists[i] = query.filter((phrase) => !compareArrays(phrase, []));
+  });
+
+  // are the anyAlls all duplicates?
+  if (allDuplicates(anyAlls)) {
+    // unique phrases
+    const uniquePhrases = [...new Set(updatedQueryLists.flat())];
+
+    // go through each phrase. if they exist in every query, remove it
     for (let phraseIndex = 0; phraseIndex < uniquePhrases.length; phraseIndex += 1) {
-      if (existsInAll(uniquePhrases[phraseIndex], queryLists)) {
-        uniquePhrases = removePhrase(uniquePhrases[phraseIndex], queryLists);
+      if (existsInAll(uniquePhrases[phraseIndex], updatedQueryLists)) {
+        updatedQueryLists = removePhrase(uniquePhrases[phraseIndex], updatedQueryLists);
       }
     }
-    return uniquePhrases[index];
+
+    // if all values are removed, return the original query
+    if (compareArrays(updatedQueryLists[index], [])) {
+      return originalQuery;
+    }
+
+    // return simplified query
+    return updatedQueryLists[index];
   }
-  return queryLists;
+  // anyAlls do not match, return original query
+  return queryLists[index];
 };
 
 export default simplifyQueryList;
