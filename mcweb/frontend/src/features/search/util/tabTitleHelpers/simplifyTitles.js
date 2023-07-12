8fea03ab2aa3ac7111c952baf9f8648160d17f37
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-12 16:01:50 -0400

    case that  'AND ' or 'OR ' is left in results

--- mcweb/frontend/src/features/search/util/tabTitleHelpers/simplifyTitles.js
+++ mcweb/frontend/src/features/search/util/tabTitleHelpers/simplifyTitles.js
@@ -1,21 +1,35 @@
-const simplifyTitles = (strings) => {
-  if (!strings.length) {
-    return [];
-  }
+import allDuplicates from './allDuplicates';
 
-  let prefix = strings[0]; // Assume the first string as the prefix initially
+const simplifyTitles = (titles, anyAlls) => {
+  if (!titles.length) return [];
+
+  if (!allDuplicates(anyAlls)) return titles;
+
+  let prefix = titles[0]; // Assume the first string as the prefix initially
 
   // Find the common prefix among all strings
-  for (let i = 1; i < strings.length; i += 1) {
-    while (!strings[i].startsWith(prefix)) {
+  for (let i = 1; i < titles.length; i += 1) {
+    while (!titles[i].startsWith(prefix)) {
       prefix = prefix.slice(0, -1);
     }
   }
 
-  // Remove the common prefix from each string
-  const modifiedStrings = strings.map((string) => string.slice(prefix.length));
+  // Remove the common prefix from each string and remove "AND " or "OR "
+  const simplifiedTitles = titles.map((title) => {
+    let modifiedTitle = title.slice(prefix.length);
 
-  return modifiedStrings;
+    if (modifiedTitle === '') {
+      modifiedTitle = title;
+    } else if (modifiedTitle.startsWith(' AND ')) {
+      modifiedTitle = modifiedTitle.slice(5); // Remove "AND " from the beginning
+    } else if (modifiedTitle.startsWith(' OR ')) {
+      modifiedTitle = modifiedTitle.slice(4); // Remove "OR " from the beginning
+    }
+
+    return modifiedTitle;
+  });
+
+  return simplifiedTitles;
 };
 
 export default simplifyTitles;
