7e6110fbc83314c70dd06ad7b4f744a62eff27f2
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-16 11:29:24 -0400

    implemented checkForBlankQuery

--- mcweb/frontend/src/features/search/util/deactivateButton.js
+++ mcweb/frontend/src/features/search/util/deactivateButton.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import isQueryListBlank from './isQueryListBlank';
+import checkForBlankQuery from './checkForBlankQuery';
 
 const isSameOrBefore = require('dayjs/plugin/isSameOrBefore');
 
@@ -10,43 +10,26 @@ const deactvateButton = (queryState) => {
 
   queryState.forEach((queryObject) => {
     const {
-      queryString,
-      queryList,
-      negatedQueryList,
       startDate,
       endDate,
       isFromDateValid,
       isToDateValid,
-      advanced,
     } = queryObject;
-
-    const totalQuery = queryList.concat(negatedQueryList);
 
     // parameters are both boolean variables
     function validFromAndToDates(validatedFromDate, validatedToDate) {
       return validatedFromDate && validatedToDate;
     }
 
-    // checks to see if the startDate is before the endDAte
+    // checks to see if the startDAte is before the endDAte
     function validDates(startingDate, endingDate) {
       return dayjs(startingDate).isSameOrBefore(dayjs(endingDate));
     }
 
-    // is the advanced search query string not just the "*"
-    function validQueryString() {
-      return advanced && queryString.trim() !== '';
-    }
-
-    // does the queryList contain any elements?
-    const isQueryValid = !isQueryListBlank(totalQuery);
-
     // are the dates in correct order and validated?
     const areDatesValid = validDates(startDate, endDate) && validFromAndToDates(isFromDateValid, isToDateValid);
 
-    // is the advanced search query not empty?
-    const isQueryStringValid = validQueryString(queryString, advanced);
-
-    returnVal = ((isQueryValid || isQueryStringValid) && areDatesValid);
+    returnVal = (!checkForBlankQuery(queryState) && areDatesValid);
   });
   return returnVal;
 };
