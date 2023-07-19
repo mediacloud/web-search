5ff4d84a8c309dc00dba97338f2443bdbfb35564
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-19 13:07:17 -0400

    mimick queryList and TabbedSearch with edit field

--- mcweb/frontend/src/features/auth/Account.jsx
+++ mcweb/frontend/src/features/auth/Account.jsx
@@ -12,6 +12,8 @@ import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import {
   Button,
   Dialog,
@@ -53,49 +55,22 @@ function Account() {
   // const [deleteUserSecret] = useDeleteUserSecretMutation();
   const [open, setOpen] = useState(false);
 
-  const [openAPI, setOpenAPI] = useState(false);
-  const [formState, setFormState] = useState({
-    apiName: '',
-    apiValue: '',
-  });
-
+  const [edit, setEdit] = useState([false]);
   const [apiList, setApiList] = useState([
     {
       apiName: 'twitter',
       apiValue: 'b3ca728ebdf19dca0f221b6b0d6e3c1fcd356879kkkjkj',
     },
   ]);
-  const [editIndex, setEditIndex] = useState(null);
-  console.log(editIndex);
 
-  const handleOpenAPI = () => {
-    setOpenAPI(true);
+  const handleAPIAdd = () => {
+    const list = [...apiList];
+    list.push({ apiName: '', apiValue: '' });
+    setApiList(list);
   };
 
-  const handleCloseAPI = () => {
-    setOpenAPI(false);
-    setFormState({ apiName: '', apiValue: '' });
-    setEditIndex(null);
-  };
-  const handleSubmitAPI = async () => {
-    if (editIndex === null) {
-      // await createUserSecret(formState).unwrap();
-    } else {
-      setApiList((prev) => prev.map((item, index) => (item.apiName === editIndex ? formState : item)));
-    }
-    handleCloseAPI();
-  };
-  const handleDeleteAPI = async (id) => {
-    setApiList(apiList.filter((item) => item.apiName !== apiName));
-    // await deleteUserSecret(id);
-  };
-  const handleEditAPI = (apiName) => {
-    const item = apiList.find((item) => item.apiName === apiName);
-    setFormState({ apiName: item.apiName, apiValue: item.apiValue });
-    setEditIndex(apiName);
-    setOpenAPI(true);
-  };
-  const handleChange = ({ target: { name, value } }) => setFormState((prev) => ({ ...prev, [name]: value }));
+  console.log(apiList);
+  // const handleChange = ({ target: { name, value } }) => setFormState((prev) => ({ ...prev, [name]: value }));
 
   return (
     <>
@@ -139,35 +114,37 @@ function Account() {
             <dd>{currentUser.isSuperuser ? 'yes' : 'no'}</dd>
           </Permissioned>
 
-          {apiList
-            && apiList.map((item) => (
-              <div key={item.apiName} style={{ display: 'flex', alignItems: 'center' }}>
-                <div style={{ paddingRight: '50px' }}>
-                  <div>
-                    <dt>
-                      {item.apiName}
-                    </dt>
-                    <dd>{mask(item.apiValue, 8, '*')}</dd>
-                  </div>
-                </div>
+          {apiList.map((item) => (
+            <div key={item.apiName} style={{ display: 'flex', alignItems: 'center' }}>
+              <div>
                 <div>
-                  <Button
-                    style={{ marginRight: '15px' }}
-                    onClick={() => handleEditAPI(item.apiName)}
-                  >
-                    Edit
-                  </Button>
-                  <Button onClick={() => handleDeleteAPI(item.apiName)}>
-                    Delete
-                  </Button>
+                  <dt>
+                    {item.apiName}
+                  </dt>
+                  <dd>{mask(item.apiValue, 8, '*')}</dd>
                 </div>
               </div>
-            ))}
+              <div style={{ paddingLeft: '25px' }}>
+                <EditIcon
+                  style={{ marginRight: '15px' }}
+                  onClick={() => { console.log('edit'); }}
+                >
+                  Edit
+                </EditIcon>
+                <RemoveCircleOutlineIcon
+                  onClick={() => { console.log('delete'); }}
+                >
+                  Delete
+                </RemoveCircleOutlineIcon>
+              </div>
+            </div>
+          ))}
         </dl>
 
-        <Button variant="outlined" color="primary" onClick={handleOpenAPI}>
+        <Button variant="outlined" color="primary" onClick={handleAPIAdd}>
           Add Service API
         </Button>
+
         <br />
         <br />
 
@@ -205,55 +182,57 @@ function Account() {
           </div>
         </Permissioned>
         {/* Modal For Updating or Adding API */}
-        <Dialog
-          open={openAPI}
-          onClose={handleCloseAPI}
-          maxWidth="md"
-          fullWidth
-        >
-          <DialogTitle>
-            {editIndex ? 'Update' : 'Add'}
-            {' '}
-            Service Specific API key
-          </DialogTitle>
-          <DialogContent>
-            <br />
-            <FormControl fullWidth>
-              <InputLabel id="type-select-label">Select API type</InputLabel>
-              <Select
-                labelId="type-select-label"
-                id="type-select"
-                name="apiName"
-                label="Select API type"
-                value={formState.apiName}
-                onChange={handleChange}
-              >
-                <MenuItem value="twitter">
-                  {platformDisplayName('twitter')}
-                </MenuItem>
-                <MenuItem value="youtube">
-                  {platformDisplayName('youtube')}
-                </MenuItem>
-              </Select>
-            </FormControl>
-            <br />
-            <br />
-            <TextField
-              name="apiValue"
-              label="API"
-              value={formState.apiValue}
-              onChange={handleChange}
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseAPI}>Cancel</Button>
-            <Button onClick={handleSubmitAPI}>Confirm</Button>
-          </DialogActions>
-        </Dialog>
+
       </div>
     </>
   );
 }
 
 export default Account;
+
+// <Dialog
+//   open={openAPI}
+//   onClose={handleCloseAPI}
+//   maxWidth="md"
+//   fullWidth
+// >
+//   <DialogTitle>
+//     {editedApiElement ? 'Update' : 'Add'}
+//     {' '}
+//     Service Specific API key
+//   </DialogTitle>
+//   <DialogContent>
+//     <br />
+//     <FormControl fullWidth>
+//       <InputLabel id="type-select-label">Select API type</InputLabel>
+//       <Select
+//         labelId="type-select-label"
+//         id="type-select"
+//         name="apiName"
+//         label="Select API type"
+//         value={formState.apiName}
+//         onChange={handleChange}
+//       >
+//         <MenuItem value="twitter">
+//           {platformDisplayName('twitter')}
+//         </MenuItem>
+//         <MenuItem value="youtube">
+//           {platformDisplayName('youtube')}
+//         </MenuItem>
+//       </Select>
+//     </FormControl>
+//     <br />
+//     <br />
+//     <TextField
+//       name="apiValue"
+//       label="API"
+//       value={formState.apiValue}
+//       onChange={handleChange}
+//       fullWidth
+//     />
+//   </DialogContent>
+//   <DialogActions>
+//     <Button onClick={handleCloseAPI}>Cancel</Button>
+//     <Button onClick={handleSubmitAPI}>Confirm</Button>
+//   </DialogActions>
+// </Dialog>
