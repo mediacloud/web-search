ec54f79db81630828e51c4df943e5e844a9a229c
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-07-20 12:01:54 -0400

    implement create api token hook with error validation in snackbar

--- mcweb/frontend/src/features/auth/Account.jsx
+++ mcweb/frontend/src/features/auth/Account.jsx
@@ -9,12 +9,12 @@ import AlertTitle from '@mui/material/AlertTitle';
 import { useSnackbar } from 'notistack';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import CancelIcon from '@mui/icons-material/Cancel';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import Button from '@mui/material/Button';
-import { useResetTokenMutation, useDeleteUserMutation } from '../../app/services/authApi';
+import Box from '@mui/material/Box';
+import { useResetTokenMutation, useDeleteUserMutation, useSaveAPIMutation } from '../../app/services/authApi';
 import Permissioned, { ROLE_STAFF } from './Permissioned';
 import { selectCurrentUser, setCredentials } from './authSlice';
 import Header from '../ui/Header';
@@ -26,7 +26,12 @@ function Account() {
   const currentUser = useSelector(selectCurrentUser);
   const [deleteUser] = useDeleteUserMutation();
   const [resetToken] = useResetTokenMutation();
+  const [saveAPI] = useSaveAPIMutation();
   const { enqueueSnackbar } = useSnackbar();
+  const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState([false]);
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const logAndRefresh = (delay) => {
     enqueueSnackbar('Token reset!', { variant: 'success' });
@@ -35,27 +40,26 @@ function Account() {
     }, delay);
   };
 
-  const [open, setOpen] = useState(false);
-  const [edit, setEdit] = useState([false]);
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const APIMenuOpen = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const [apiList, setApiList] = useState([
     {
       apiName: 'twitter',
-      apiValue: 'b3ca728ebdf19dca0f221b6b0d6e3c1fcd356879kkkjkj',
+      apiKey: 'b3ca728ebdf19dca0f221b6b0d6e3c1fcd356879kkkjkj',
     },
   ]);
 
+  const handleClick = (event, index) => {
+    setValue(index);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (index, apiName) => {
+    const updatedApiList = [...apiList];
+    updatedApiList[index].apiName = apiName;
+    setApiList(updatedApiList);
+    setAnchorEl(null);
+  };
+
   const handleAPIAdd = () => {
-    setApiList(() => [...apiList, { apiName: 'API Name', apiValue: 'API Key' }]);
+    setApiList(() => [...apiList, { apiName: 'API Name', apiKey: 'API Key' }]);
     setEdit(() => [...edit, true]);
   };
 
@@ -63,10 +67,6 @@ function Account() {
     setApiList(apiList.filter((_, i) => i !== index));
     setEdit(edit.filter((_, i) => i !== index));
   };
-
-  console.log(apiList);
-  console.log(edit);
-  // const handleChange = ({ target: { name, value } }) => setFormState((prev) => ({ ...prev, [name]: value }));
 
   return (
     <>
@@ -111,99 +111,72 @@ function Account() {
           </Permissioned>
 
           {apiList.map((item, index) => (
-            <div style={{ marginBottom: '20px' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
               {
                 !edit[index] && (
                   <div key={item.apiName} style={{ display: 'flex', alignItems: 'center' }}>
                     <div>
-                      <dt>
-                        {item.apiName}
-                      </dt>
-                      <dd>{mask(item.apiValue, 8, '*')}</dd>
+                      <dt>{item.apiName}</dt>
+                      <dd>{mask(item.apiKey)}</dd>
                     </div>
-                    <EditIcon
-                      sx={{ marginLeft: '35px' }}
-                      onClick={() => {
-                        const updatedEdit = [...edit];
-                        updatedEdit[index] = true;
-                        setEdit(updatedEdit);
-                      }}
-                    >
-                      Edit
-                    </EditIcon>
-                    {apiList.length > 1 && (
-                      <RemoveCircleOutlineIcon
-                        sx={{ marginLeft: '15px' }}
-                        onClick={() => handleAPIRemove(index)}
-                      >
-                        Delete
-                      </RemoveCircleOutlineIcon>
-                    )}
                   </div>
                 )
               }
               {edit[index] && (
                 <div>
                   <Button
-                    id="basic-button"
-                    aria-controls={APIMenuOpen ? 'basic-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={APIMenuOpen ? 'true' : undefined}
-                    onClick={handleClick}
+                    onClick={(event) => handleClick(event, index)}
                   >
-                    API Services
+                    {apiList[index].apiName === 'API Name' ? 'API NAME' : apiList[index].apiName}
                   </Button>
-                  <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={APIMenuOpen}
-                    onClose={handleClose}
-                  >
-                    <MenuItem onClick={handleClose}>Reddit</MenuItem>
-                    <MenuItem onClick={handleClose}>Twitter</MenuItem>
-                    <MenuItem onClick={handleClose}>Youtube</MenuItem>
+                  <Menu key={apiList[index].apiName + apiList[index].apiKey} anchorEl={anchorEl} open={Boolean(anchorEl)}>
+                    <MenuItem onClick={() => handleClose(value, 'Reddit')}>Reddit</MenuItem>
+                    <MenuItem onClick={() => handleClose(value, 'Youtube')}>Youtube</MenuItem>
                   </Menu>
                   {/* input for customizing api key */}
                   <input
                     style={{ width: '400px' }}
-                    value={apiList[index].apiValue}
+                    value={apiList[index].apiKey}
                     type="text"
                     onChange={(event) => {
                       const updatedValues = [...apiList];
-                      updatedValues[index].apiValue = event.target.value;
+                      updatedValues[index].apiKey = event.target.value;
                       setApiList(updatedValues);
                     }}
                   />
+                  {/* Confirm */}
                   <CheckIcon
-                    disabled={apiList[index].apiValue.length === 0}
+                    disabled={apiList[index].apiKey.length === 0}
                     sx={{ marginLeft: '35px' }}
-                    onClick={() => {
-                      const updatedEdit = [...edit];
-                      updatedEdit[index] = false;
-                      setEdit(updatedEdit);
+                    onClick={async () => {
+                      try {
+                        await saveAPI({ apiData: apiList[index], username: currentUser.username }).unwrap();
+                        const updatedEdit = [...edit];
+                        updatedEdit[index] = false;
+                        setEdit(updatedEdit);
+                      } catch (err) {
+                        enqueueSnackbar(`Saving API Key - ${err.data.error}`, { variant: 'error' });
+                      }
                     }}
                   />
-                  {apiList.length > 1 && (
-                    <RemoveCircleOutlineIcon
-                      sx={{ marginLeft: '15px' }}
-                      onClick={() => handleAPIRemove(index)}
-                    >
-                      Delete
-                    </RemoveCircleOutlineIcon>
-                  )}
+                  {/* Remove */}
                 </div>
               )}
-            </div>
+              <RemoveCircleOutlineIcon
+                sx={{ marginLeft: '15px' }}
+                onClick={() => handleAPIRemove(index)}
+              >
+                Delete
+              </RemoveCircleOutlineIcon>
+            </Box>
           ))}
         </dl>
 
         <Button variant="outlined" color="primary" onClick={handleAPIAdd}>
           Add Service API
         </Button>
-
         <br />
         <br />
-
         <Alert severity="error">
           <AlertTitle>Delete Account</AlertTitle>
           <AlertDialog
@@ -237,58 +210,9 @@ function Account() {
             </div>
           </div>
         </Permissioned>
-        {/* Modal For Updating or Adding API */}
-
       </div>
     </>
   );
 }
 
 export default Account;
-
-// <Dialog
-//   open={openAPI}
-//   onClose={handleCloseAPI}
-//   maxWidth="md"
-//   fullWidth
-// >
-//   <DialogTitle>
-//     {editedApiElement ? 'Update' : 'Add'}
-//     {' '}
-//     Service Specific API key
-//   </DialogTitle>
-//   <DialogContent>
-//     <br />
-//     <FormControl fullWidth>
-//       <InputLabel id="type-select-label">Select API type</InputLabel>
-//       <Select
-//         labelId="type-select-label"
-//         id="type-select"
-//         name="apiName"
-//         label="Select API type"
-//         value={formState.apiName}
-//         onChange={handleChange}
-//       >
-//         <MenuItem value="twitter">
-//           {platformDisplayName('twitter')}
-//         </MenuItem>
-//         <MenuItem value="youtube">
-//           {platformDisplayName('youtube')}
-//         </MenuItem>
-//       </Select>
-//     </FormControl>
-//     <br />
-//     <br />
-//     <TextField
-//       name="apiValue"
-//       label="API"
-//       value={formState.apiValue}
-//       onChange={handleChange}
-//       fullWidth
-//     />
-//   </DialogContent>
-//   <DialogActions>
-//     <Button onClick={handleCloseAPI}>Cancel</Button>
-//     <Button onClick={handleSubmitAPI}>Confirm</Button>
-//   </DialogActions>
-// </Dialog>
