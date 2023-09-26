cee0b8479f64129ecb64a58ef72b4f622de39458
Evan-Leon <evan.leon.j@gmail.com>
2023-09-26 15:16:06 -0400

    Make legacy and new provider changes to backend

--- mcweb/backend/search/utils.py
+++ mcweb/backend/search/utils.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from django.apps import apps
 from mc_providers import provider_name, PLATFORM_TWITTER, PLATFORM_SOURCE_TWITTER, PLATFORM_YOUTUBE,\
     PLATFORM_SOURCE_YOUTUBE, PLATFORM_REDDIT, PLATFORM_SOURCE_PUSHSHIFT, PLATFORM_SOURCE_MEDIA_CLOUD,\
-    PLATFORM_SOURCE_WAYBACK_MACHINE, PLATFORM_ONLINE_NEWS
+    PLATFORM_SOURCE_WAYBACK_MACHINE, PLATFORM_ONLINE_NEWS, PLATFORM_SOURCE_MEDIA_CLOUD_LEGACY
 
 
 def fill_in_dates(start_date, end_date, existing_counts):
@@ -66,6 +66,8 @@ def search_props_for_provider(provider, collections: List, sources: List) -> Dic
     if provider == provider_name(PLATFORM_ONLINE_NEWS, PLATFORM_SOURCE_WAYBACK_MACHINE):
         return _for_wayback_machine(collections, sources)
     if provider == provider_name(PLATFORM_ONLINE_NEWS, PLATFORM_SOURCE_MEDIA_CLOUD):
+        return _for_wayback_machine(collections, sources)
+    if provider == provider_name(PLATFORM_ONLINE_NEWS, PLATFORM_SOURCE_MEDIA_CLOUD_LEGACY):
         return _for_media_cloud(collections, sources)
     return {}
 
