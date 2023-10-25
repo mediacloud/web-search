0d8490263f37299d262955453ca322e9fd8eb139
Evan-Leon <evan.leon.j@gmail.com>
2023-10-25 09:39:16 -0400

    Begin methods to show source or collection volume over time

--- mcweb/frontend/src/features/collections/CollectionShow.jsx
+++ mcweb/frontend/src/features/collections/CollectionShow.jsx
@@ -3,11 +3,21 @@ import { useParams } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 import SourceList from '../sources/SourceList';
 import { useGetCollectionQuery } from '../../app/services/collectionsApi';
+import { useGetCountOverTimeMutation } from '../../app/services/searchApi';
+import CountOverTimeChart from '../search/results/CountOverTimeChart';
 
 export default function CollectionShow() {
   const params = useParams();
   const collectionId = Number(params.collectionId);
-
+  // const [dispatchQuery, { isLoading: countLoading, data, error }] = useGetCountOverTimeMutation();
+  // const preparedData = prepareCountOverTimeData(data, normalized, queryState);
+  // if (preparedData.length !== queryState.length) return null;
+  // const updatedPrepareCountOverTimeData = preparedData.map(
+  //   (originalDataObj, index) => {
+  //     const queryTitleForPreparation = { name: queryState[index].name };
+  //     return { ...queryTitleForPreparation, ...originalDataObj };
+  //   },
+  // );
   const {
     data: collection,
     isLoading,
@@ -25,6 +35,10 @@ export default function CollectionShow() {
             <b>Notes:</b> {collection.notes}
           </p>
           <SourceList collectionId={collectionId} />
+          <CountOverTimeChart
+            series={updatedPrepareCountOverTimeData}
+            normalized={false}
+          />
         </div>
       </div>
     </div>
