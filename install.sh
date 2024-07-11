a6297aadb597329c874d8ba8ebdd58ecf8ba04be
Paige Gulley <nano3.14@gmail.com>
2024-07-10 23:22:10 -0400

    Update install.sh - removed foolishness

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Running migrations and building javacsript"
-python mcweb.manage.py makemigrations
-python mcweb.manage.py migrate
+python mcweb/manage.py makemigrations
+python mcweb/manage.py migrate
 npm run build
 python mcweb/manage.py collectstatic --noinput
 echo "  done with migrations and javascript build"
@@ -10,13 +10,16 @@ echo "  done with migrations and javascript build"
 
 ###get the config variables from github (avoiding referencing directly)
 #This is just for reference
-echo $STACK_NAME
+
 
 if [ -z "$STACK_NAME"]; then
     echo "STACK_NAME is not set, using local .env"
 else
+    echo "Attempting to deploy with stack name: $STACK_NAME"
     if [ -z ${GITHUB_USR} || -z ${GITHUB_PAT} ]; then
-        echo "Can't deploy $STACK_NAME without setting GITHUB_USR and GITHUB_PAT"
+        echo "wont deploy $STACK_NAME without a GITHUB_USR and GITHUB_PAT"
+        exit 1
+    fi
     echo "deploying $STACK_NAME stack"
     
     https://$GITHUB_USR:$GITHUB_PAT>@github.com/mediacloud/web-search-config.git
