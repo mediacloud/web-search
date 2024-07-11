d14dec358b9709ee1f236521ffb1ad3eb04f010a
Paige Gulley <nano3.14@gmail.com>
2024-07-10 23:42:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,17 @@ echo "  done with migrations and javascript build"
 #This is just for reference
 
 
-if [ -z "$STACK_NAME"]; then
+if [ -z "$STACK_NAME" ]; then
     echo "STACK_NAME is not set, using local .env"
 else
     echo "Attempting to deploy with stack name: $STACK_NAME"
-    if [ -z ${GITHUB_USR} || -z ${GITHUB_PAT} ]; then
+    if [ -z ${GITHUB_USR} -o -z ${GITHUB_PAT} ]; then
         echo "wont deploy $STACK_NAME without a GITHUB_USR and GITHUB_PAT"
         exit 1
     fi
     echo "deploying $STACK_NAME stack"
     
-    https://$GITHUB_USR:$GITHUB_PAT>@github.com/mediacloud/web-search-config.git
+    git clone https://"$GITHUB_USR":"$GITHUB_PAT">@github.com/mediacloud/web-search-config.git
     cp "web-search-config/web-search.$STACK_NAME.sh" mcweb/.env
 fi
 
