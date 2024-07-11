a66323a9b914fe11d554297b4dd0ad3e85460422
Paige Gulley <nano3.14@gmail.com>
2024-07-10 21:24:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,22 +10,14 @@ echo "  done with migrations and javascript build"
 
 ###get the config variables from github (avoiding referencing directly)
 #This is just for reference
-printenv > /tmp/dokku_predeploy_env.txt
-echo $(printenv)
+echo $STACK_NAME
 
-if [ -z "$DOKKU_APP_NAME" ]; then
-    echo "DOKKU_APP_NAME is not set"
-    exit 1
+if [ -z "$STACK_NAME" ]; then
+    echo "STACK_NAME is not set, using local .env"
 else
-    echo "DOKKU_APP_NAME is set to $DOKKU_APP_NAME"
-    if [[ "$DOKKU_APP_NAME" == "mcweb" ]]; then
-    	ENV_TYPE="prod"
-    elif [[ "$DOKKU_APP_NAME" == "mcweb-staging" ]]; then
-    	ENV_TYPE="staging"
-    fi
-
+    echo "deploying $STACK_NAME stack"
+    git clone git@github.com:mediacloud/web-search-config.git
+    cp "web-search-config/web-search.$STACK_NAME.sh" mcweb/.env
 fi
 
-git clone git@github.com:mediacloud/web-search-config.git
 
-cp "web-search-config/web-search.$ENV_TYPE.sh" mcweb/.env
