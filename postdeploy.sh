3b3a5051536b53af4e92d6c15e802d6ba97c76d0
Paige Gulley <nano3.14@gmail.com>
2024-07-11 14:48:05 -0400

    export?

--- postdeploy.sh
+++ postdeploy.sh
@@ -6,8 +6,9 @@ else
     echo "Attempting to apply environment for: $STACK_NAME"
     tail web-search-config/websearch.$STACK_NAME.sh 
     . web-search-config/web-search.$STACK_NAME.sh
-    echo $AIRTABLE_API_KEY
-fi
+    export $AIRTABLE_API_KEY
+    export $MEAG_BASE_ID
+    HOSTNAME=$(hostname --short)
+    python -m mc-manage.airtable-deployment-update --codebase 'web-search' --name $STACK_NAME --env $STACK_NAME --version "latest" --hardware $HOSTNAME
 
-HOSTNAME=$(hostname --short)
-python -m mc-manage.airtable-deployment-update --codebase 'web-search' --name $STACK_NAME --env $STACK_NAME --version "latest" --hardware $HOSTNAME
+fi
