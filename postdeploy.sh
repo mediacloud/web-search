ba2af935164b297acecf1398ba43427adc727514
Paige Gulley <nano3.14@gmail.com>
2024-07-10 23:33:18 -0400

    Update postdeploy.sh

--- postdeploy.sh
+++ postdeploy.sh
@@ -2,4 +2,4 @@
 HOSTNAME=$(hostname --short)
 BRANCH=$(git branch --show-current)
 VERSION=$(git describe --tags)
-python -m mc-manage.airtable-deployment-update --codebase 'web-search' --name $BRANCH --env $ENV_NAME --version $VERSION --hardware $HOSTNAME
+python -m mc-manage.airtable-deployment-update --codebase 'web-search' --name $BRANCH --env $STACK_NAME --version $VERSION --hardware $HOSTNAME
