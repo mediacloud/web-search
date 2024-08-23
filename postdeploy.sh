c72073a6e7129a57e441207e371f2209d0e636be
Phil Budne <pbudne@angwin.cs.umass.edu>
2024-08-22 23:57:30 -0400

    postdeploy.sh: simplify

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 if [ "x$AIRTABLE_API_KEY" != x ]; then
-    WS='[ 	]*'
-    Q="['\"]"
-    VERSION=$(grep '^VERSION' mcweb/settings.py | sed -e "s/^VERSION${WS}=${WS}$Q//" -e "s/$Q.*$//")
+    # try extracting version from mcweb.settings, suppress "not configured" log messages
+    VERSION=$(python -c 'from mcweb.settings import VERSION; print(VERSION)') 2>&1 | grep -v 'not configured'
 
     echo TEMP-- should all be supplied via Dokku config:
     echo "AIRTABLE_API_KEY=$AIRTABLE_API_KEY"
@@ -13,11 +12,12 @@ if [ "x$AIRTABLE_API_KEY" != x ]; then
     echo "AIRTABLE_NAME=$AIRTABLE_NAME"
     echo "VERSION=$VERSION"
 
-    AIRTABLE_API_KEY=$AIRTABLE_API_KEY MEAG_BASE_ID=$AIRTABLE_BASE_ID \
-		    python -m mc-manage.airtable-deployment-update \
+    # AIRTABLE_API_KEY used w/o change
+    MEAG_BASE_ID=$AIRTABLE_BASE_ID \
+		python -m mc-manage.airtable-deployment-update \
 		    --codebase 'web-search' \
 		    --env "$AIRTABLE_ENV" \
 		    --hardware "$AIRTABLE_HARDWARE" \
 		    --name "$AIRTABLE_NAME" \
 		    --version "$VERSION"
-fi 
+fi
