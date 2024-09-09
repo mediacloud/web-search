9a4bd058f51e4e59353ee1bbebbe8a631fbefce1
Evan-Leon <evan.leon.j@gmail.com>
2024-09-09 09:48:58 -0400

    Update to new version

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -24,7 +24,7 @@ from django.core.exceptions import ImproperlyConfigured
 logger = logging.getLogger(__file__)
 
 # The static version of the app
-VERSION = "2.0.5"
+VERSION = "2.0.6"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
