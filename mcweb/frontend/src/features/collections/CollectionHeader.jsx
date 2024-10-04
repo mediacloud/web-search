1c98efd616b936b3b8a7b1e3cbe84454283691b6
Evan-Leon <evan.leon.j@gmail.com>
2024-10-04 11:18:00 -0400

    Add tooltip to static chip

--- mcweb/frontend/src/features/collections/CollectionHeader.jsx
+++ mcweb/frontend/src/features/collections/CollectionHeader.jsx
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import ShieldIcon from '@mui/icons-material/Shield';
 import SearchIcon from '@mui/icons-material/Search';
 import Chip from '@mui/material/Chip';
+import Tooltip from '@mui/material/Tooltip';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { Outlet, Link, useParams } from 'react-router-dom';
 import {
@@ -64,7 +65,9 @@ export default function CollectionHeader() {
           <Chip label="Featured Collection" color="success" />
         )}
         {collection.static && (
-          <Chip label="Static Collection" color="warning" />
+          <Tooltip title="This is a static collection, to make any changes contact an admin">
+            <Chip label="Static Collection" color="warning" />
+          </Tooltip>
         )}
       </Header>
       <ControlBar>
