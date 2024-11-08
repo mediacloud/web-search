c7c98e42c67934cd65d5de7761282280675a4343
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-08 14:58:08 -0500

    moved logging_middleware into the search app, as the models need someplace to live, and that's the intended scope here

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -183,7 +183,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "util.logging_middleware.RequestLoggingMiddleware"
+    "backend.search.logging_middleware.RequestLoggingMiddleware"
 ]
 
 CORS_ALLOWED_ORIGINS = [
