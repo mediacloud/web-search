37390bf3e90628d5d6fc158929b3af62d49c2fb5
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-13 01:35:56 -0500

    i said no typing yet

--- mcweb/core/utils.py
+++ mcweb/core/utils.py
@@ -2,7 +2,6 @@ from .models import ConfigProperty
 
 def get_config_value(key):
     try:
-        config_entry = ConfigProperty.objects.get(property_name=key)
-        return config_entry.get_typed_value()
+        return ConfigProperty.objects.get(property_name=key).property_value
     except ConfigProperty.DoesNotExist:
         raise ValueError(f"Configuration key '{key}' is missing and no default is set.")
