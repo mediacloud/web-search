64f2d65e5834dacc92b466881123320188b3d61c
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-13 01:50:24 -0500

    ...

--- mcweb/core/admin.py
+++ mcweb/core/admin.py
@@ -4,7 +4,7 @@ from .models import ConfigProperty
 
 @admin.register(ConfigProperty)
 class ConfigPropertyAdmin(admin.ModelAdmin):
-	list_display = ("property_name", "property_value")
+    list_display = ("property_name", "property_value")
     readonly_fields = ("property_name",)  # Make property name read-only
 
     def has_add_permission(self, request):
