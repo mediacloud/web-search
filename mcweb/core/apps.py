e4cdd0c6711b910ef0fd5733ab2654a9e8d20636
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-13 01:04:17 -0500

    collectstatic

--- mcweb/core/apps.py
+++ mcweb/core/apps.py
@@ -1,11 +1,14 @@
 from django.apps import AppConfig
 from django.core.management import call_command
+import sys
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     #This command reads the values of CONFIG_DEFAULTS in settings.py into the database. 
+    #But only when the app is actually deployed- db isn't always ready in other contexts.
     def ready(self):
-        call_command("initialize_config")
+        if 'collectstatic' not in sys.argv:
+            call_command("initialize_config")
 
