fc2fb6a011516a313562c81abd083176ba749dfb
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-14 12:43:07 -0500

    better admin property

--- mcweb/core/admin.py
+++ mcweb/core/admin.py
@@ -4,29 +4,45 @@ from django.forms import widgets
 from .models import ConfigProperty
 
 class ConfigPropertyForm(forms.ModelForm):
+    # Define a field for displaying property name alongside property value
+    property_display = forms.Field(label="", required=False)
+
     class Meta:
         model = ConfigProperty
-        fields = ["property_name", "property_value", "property_type"]
+        fields = ["property_display", "property_type"]  # Hide raw property_name, property_value
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Set widget based on property_type
-        if self.instance.property_type == "bool":
-            self.fields["property_value"].widget = forms.CheckboxInput()
-        elif self.instance.property_type == "int":
-            self.fields["property_value"].widget = forms.NumberInput()
-        else:
-            self.fields["property_value"].widget = forms.TextInput()
 
-    def clean_property_value(self):
-    	#the value is serialized as a string
-        value = self.cleaned_data["property_value"]
-        # Convert the input based on property_type before saving
-        if self.instance.property_type == "bool":
+        # Dynamically set the widget and label for property_display based on the instance
+        self.fields["property_display"].label = self.instance.property_name
+        self.fields["property_display"].widget = self.get_widget_for_property_type()
+
+    def get_widget_for_property_type(self):
+        """Returns the appropriate widget based on property_type."""
+        if self.instance.property_type == "boolean":
+            return forms.CheckboxInput()
+        elif self.instance.property_type == "integer":
+            return forms.NumberInput()
+        elif self.instance.property_type == "date":
+            return forms.DateInput(attrs={"type": "date"})
+        return forms.TextInput()
+
+    def clean_property_display(self):
+        # Transform the cleaned display field input to be saved in property_value
+        value = self.cleaned_data["property_display"]
+        if self.instance.property_type == "boolean":
             return str(bool(value))  # Store as "True" or "False"
-        elif self.instance.property_type == "int":
+        elif self.instance.property_type == "integer":
             return str(int(value))   # Store as a string representing an integer
-        return value  # Default to string as-is for other types
+        elif self.instance.property_type == "date":
+            return value.strftime("%Y-%m-%d")  # Convert date to "YYYY-MM-DD"
+        return value
+
+    def save(self, commit=True):
+        # Save the cleaned display field to property_value
+        self.instance.property_value = self.cleaned_data["property_display"]
+        return super().save(commit=commit)
 
 
 @admin.register(ConfigProperty)
