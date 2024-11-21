b025e5dc99fd46ccbd4d8413f9d55c3a6eafdea7
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-11-21 13:03:55 -0500

    excluded headers

--- mcweb/core/logging_middleware.py
+++ mcweb/core/logging_middleware.py
@@ -49,7 +49,12 @@ class RequestLoggingMiddleware:
             log_msg["params"] = request_params
             log_msg["duration"] = duration
             log_msg["request_time"] = start_time
-            log_msg["headers"] = {key: value for key, value in request.headers.items()}
+            
+            exclude_headers = ["Cookie", "X-Csrftoken", "Referer"]
+            log_msg["headers"] = {key: value for key, value in request.headers.items() if key not in exclude_headers}
+
+
+
             # Log the request details
             request_logger.info(json.dumps(log_msg))
         return response
