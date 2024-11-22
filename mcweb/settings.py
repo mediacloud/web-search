774c1230f1e26a82d814af3b19ec4ea525872e8e
Paige Gulley <nano3.14@gmail.com>
2024-11-22 12:38:29 -0500

    redis connection strat?

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -400,7 +400,7 @@ DISABLE_SERVER_SIDE_CURSORS = True
 CONSTANCE_BACKEND = 'constance.backends.redisd.CachingRedisBackend'
 # optionally set a value ttl
 CONSTANCE_REDIS_CACHE_TIMEOUT = 60
-CONSANCE_REDIS_CONNECTION = CACHES['default']['LOCATION']
+CONSTANCE_REDIS_CONNECTION = 'django_redis.get_redis_connection'
 
 CONSTANCE_CONFIG = {
     "REQUEST_LOGGING_ENABLED": (False, 'Request logging enabled', bool),
