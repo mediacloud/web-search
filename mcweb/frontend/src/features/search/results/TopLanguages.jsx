78b63e11196bb6815cc8e5e5322c00f2dec259de
Evan Leon <78226696+Evan-Leon@users.noreply.github.com>
2024-12-23 10:02:22 -0500

    Less text on search errors (#889)
    * Try to catch timeout exception, console log error
    
    * Change exception catching in backend and error note for front end results components
    
    * Update handle provider errors to handle the runtimeerror and connection error
    
    * Subtract latest searchable day to subtract one day from today
    
    * Fix for dom nesting warnings in console
    
    * Fix various warnings in console

--- mcweb/frontend/src/features/search/results/TopLanguages.jsx
+++ mcweb/frontend/src/features/search/results/TopLanguages.jsx
@@ -61,7 +61,7 @@ export default function TopLanguages() {
       <Alert severity="warning">
         Sorry, but something went wrong.
         (
-        {error ? error.note : 'No results please try a different query'}
+        {error ? error.detail : 'No results please try a different query'}
         )
       </Alert>
     );
