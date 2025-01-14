6480d22dc99c0e6ec5c2e4630b78758fa8ae152e
Evan-Leon <evan.leon.j@gmail.com>
2025-01-14 15:35:33 -0500

    Remove comments

--- mcweb/frontend/src/features/sources/UploadSources.jsx
+++ mcweb/frontend/src/features/sources/UploadSources.jsx
@@ -14,11 +14,6 @@ export default function UploadSources({ collectionId, rescrape, managedCollectio
   const [uploadSources, { isLoading: isUpdating }] = useUploadSourcesMutation();
 
   const { CSVReader } = useCSVReader();
-  // const handleOnDrop = (data) => {
-  //   const cleanedData = data.map((row) => row.map((cell) => (cell === '' ? null : cell)));
-  //   setCsvData(cleanedData);
-  //   console.log(csvData);
-  // };
 
   return (
     <div>
@@ -27,7 +22,6 @@ export default function UploadSources({ collectionId, rescrape, managedCollectio
           header: true,
           delimiter: ',',
         }}
-        // onDrop={handleOnDrop}
         onUploadAccepted={async (uploadInfo) => {
           setUpdating(true);
           console.log(uploadInfo);
