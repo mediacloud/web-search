e76e812afa4da8895e84d983b8e71db1abf65694
Evan-Leon <evan.leon.j@gmail.com>
2025-01-14 16:28:14 -0500

    Update version and release notes

--- mcweb/frontend/src/features/sources/UploadSources.jsx
+++ mcweb/frontend/src/features/sources/UploadSources.jsx
@@ -24,7 +24,7 @@ export default function UploadSources({ collectionId, rescrape, managedCollectio
         }}
         onUploadAccepted={async (uploadInfo) => {
           setUpdating(true);
-          console.log(uploadInfo, 'Upload Info');
+          // console.log(uploadInfo, 'Upload Info');
           const results = await uploadSources({ sources: uploadInfo.data, collection_id: collectionId, rescrape });
           setUpdating(false);
           enqueueSnackbar(
