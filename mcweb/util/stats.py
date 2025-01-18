3f6fe14915c23bad327c98d53f66b598911a4846
Phil Budne <phil@regressive.org>
2025-01-17 22:59:14 -0500

    wrap2

--- mcweb/util/stats.py
+++ mcweb/util/stats.py
@@ -54,6 +54,9 @@ class Stats:
         # XXX actual timing call here
 
     def wrap(self, func):
+        return func
+
+    def wrap2(self, func):
         """
         decorator for timing/stats
         needs to be after any decorators
@@ -67,7 +70,6 @@ class Stats:
         function nesting here, and for no immediate benefit.
         """
         # if this fires, a decorator after this one didn't preserve __name__
-        """
         assert func.__name__ not in ["view", "decorator", "_handler"]
 
         @wraps(func)
@@ -85,5 +87,3 @@ class Stats:
                 self.count("success", func.__name__) # gives rate
             return ret
         return decorator
-        """
-        return func
