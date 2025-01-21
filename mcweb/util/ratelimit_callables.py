5c7cb2d4cb292d4d26398e76b54dea8cb1a31b8f
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2025-01-21 17:57:34 -0500

    no paren

--- mcweb/util/ratelimit_callables.py
+++ mcweb/util/ratelimit_callables.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import Group
 
 #A ratelimit callable which sets a higher ratelimit if the user is staff.
 def story_list_rate(group, request):
-    if request.user.groups.filter(name="power-users").exists() or request.user.is_staff(): 
+    if request.user.groups.filter(name="power-users").exists() or request.user.is_staff: 
         return "100/m"
     else:
         return "2/m"
