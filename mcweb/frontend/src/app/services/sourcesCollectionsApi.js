17e4938e736bbb5657c5b5c941fa84a613ccd79f
Evan-Leon <evan.leon.j@gmail.com>
2025-02-06 15:08:31 -0500

    Remove frontend unused function

--- mcweb/frontend/src/app/services/sourcesCollectionsApi.js
+++ mcweb/frontend/src/app/services/sourcesCollectionsApi.js
@@ -10,14 +10,6 @@ export const sourcesCollectionsApi = managerApi.injectEndpoints({
       }),
       invalidatesTags: ['Collection', 'Source'],
     }),
-    createManySCAssociations: builder.mutation({
-      query: (payload) => ({
-        url: 'sources-collections/many-assoc',
-        method: 'POST',
-        body: { source_ids: payload.source_id, collection_id: payload.collection_id },
-      }),
-      invalidatesTags: ['Collection', 'Source'],
-    }),
     deleteSourceCollectionAssociation: builder.mutation({
       query: (ids) => ({
         url: `sources-collections/${ids.source_id}/?collection_id=${ids.collection_id}`,
@@ -31,5 +23,4 @@ export const sourcesCollectionsApi = managerApi.injectEndpoints({
 export const {
   useCreateSourceCollectionAssociationMutation,
   useDeleteSourceCollectionAssociationMutation,
-  useCreateManySCAssociationsMutation,
 } = sourcesCollectionsApi;
