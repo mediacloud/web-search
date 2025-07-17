6c7da9fb96aeb5b349f8b5a390463cb793755014
Evan Leon <78226696+Evan-Leon@users.noreply.github.com>
2025-07-17 11:53:45 -0400

    Fix for error in modify source (#1138)
    * Fix for modify source error
    
    * Additional change to ensure validation and no error

--- mcweb/backend/sources/serializer.py
+++ mcweb/backend/sources/serializer.py
@@ -81,11 +81,14 @@ class SourceSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("name cannot end with '/'")
         homepage = self.initial_data["homepage"]
         canonical_domain = urls.canonical_domain(homepage)
-        domain_exists = Source.domain_exists(canonical_domain)
-        if domain_exists:
-            raise serializers.ValidationError(f"domain {value} already exists for domain {canonical_domain}")
         if canonical_domain != value:
             raise serializers.ValidationError(f"domain {value} does not match the canonicalized version of homepage: {homepage}")
+        queryset = Source.objects.filter(name=value)
+        if self.instance:
+            queryset = queryset.exclude(pk=self.instance.pk)
+    
+        if queryset.exists():
+            raise serializers.ValidationError(f"The name '{value}' is already taken.")
         return value
     
     
