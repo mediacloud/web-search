b90b3331223701448b57cde53f45665282a1a1a3
Evan Leon <78226696+Evan-Leon@users.noreply.github.com>
2025-07-17 09:12:25 -0400

    ResetToken cleaning background command (#1135)
    * Add delete reset token command and update periodic numbers for past tasks
    
    * Change reset time to 1 day instead of 1 week
    
    * Turn token cleanup into background task

---
+++ mcweb/backend/users/management/commands/delete-reset-tokens.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+import logging
+from settings import ADMIN_USERNAME
+from ...tasks import cleanup_reset_codes
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'Delete reset codes older than one day'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--days',
+            type=int,
+            default=1,
+            help='Number of days to keep reset codes (default: 1)',
+        )
+
+    def handle(self, *args, **options):
+        logger.info(f"Starting delete-reset-tokens command (days={options['days']}), ALERTS_TASK_USERNAME={ADMIN_USERNAME}")
+        cleanup_reset_codes(days=options['days'])
+       
