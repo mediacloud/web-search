443c1f4051e9463b07cce25bf3d708fcd5b0abdb
Evan-Leon <evan.leon.j@gmail.com>
2025-11-18 10:29:04 -0500

    Show real quota for MC on profile

--- mcweb/frontend/src/features/auth/Account.jsx
+++ mcweb/frontend/src/features/auth/Account.jsx
@@ -58,7 +58,7 @@ function Account() {
   if (error) {
     enqueueSnackbar('There was an error sending the email, please try again.', { variant: 'error' });
   }
-
+  console.log(currentUser);
   return (
     <>
       <Header>
@@ -172,7 +172,8 @@ function Account() {
         <div className="row" style={{ marginTop: '20px' }}>
           <div className="col-4">
             <Alert severity="info">
-              Quota is 4000 hits per week.
+              {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+              Quota is {currentUser.quotaLimit} hits per week.
             </Alert>
           </div>
         </div>
