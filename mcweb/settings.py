22f79effa6af186ae5516c1ddd1f6f16a1184230
Evan-Leon <evan.leon.j@gmail.com>
2025-12-01 09:34:17 -0500

    Bump version and add release notes

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -24,7 +24,7 @@ from django.core.exceptions import ImproperlyConfigured
 logger = logging.getLogger(__file__)
 
 # The static version of the app
-VERSION = "2.4.0"
+VERSION = "2.4.1"
 
 class Groups:
     CONTRIBUTOR = "contributor"
