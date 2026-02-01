b139bdb89f497f651155a050ac039684fdba4818
Phil Budne <phil@regressive.org>
2026-02-01 14:34:44 -0500

    Make search manage command use CollectionViewSet and SourcesViewSet
    Created in March 2025 as a development environment for keyword search,
    but now the real code lives in the viewsets!

--- mcweb/backend/sources/management/commands/search.py
+++ mcweb/backend/sources/management/commands/search.py
@@ -1,49 +1,73 @@
+"""
+Created in March 2025 as a development environment for keyword search,
+Gutted in Feb 2026 to use CollectionViewSet and SourcesViewSet
+(since that's where the real code lives)
+"""
+
 import time
 
+from django.contrib.auth.models import User
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.core.management.base import BaseCommand, CommandError
+from rest_framework.test import APIRequestFactory, force_authenticate
 
+from settings import SYSTEM_TASK_USERNAME, ALLOWED_HOSTS
 from ...models import Collection, Source
-
-# help using search_vector column from
-# https://medium.com/@nandagopal05/django-full-text-search-with-postgresql-f063aaf34e35
-# code at https://github.com/NandaGopal56/Django-full-text-search
+from ...api import CollectionViewSet, SourcesViewSet
 
 class Command(BaseCommand):
     help = 'Search sources and collections'
 
     def add_arguments(self, parser):
-        parser.add_argument("--no-index", action="store_true", default=False)
-        parser.add_argument("--show-query", action="store_true")
+        parser.add_argument("--user", default=SYSTEM_TASK_USERNAME, type=str)
         parser.add_argument("token", nargs="+", type=str)
 
     def handle(self, *args, **options):
-        v = SearchVector("name", "label")
-        q = SearchQuery(" ".join(options["token"]))
-        r = SearchRank(v, q)
+        ALLOWED_HOSTS.append("testserver")
+
+        # generic preparation, don't charge for time spent:
+        factory = APIRequestFactory()
+        tokens = "+".join(options["token"]) # XXX full URL encoding!!
+        user = User.objects.get(username=options["user"])
+
+        # https://search.mediacloud.org/api/sources/sources/?limit=100&name=york+times+new
+        src_view = SourcesViewSet.as_view(actions={'get': 'list'})
+        src_req = factory.get(f'/sources/sources/?limit=100&name={tokens}')
+        force_authenticate(src_req, user=user)
+
 
         print("Sources")
-        print("rank | id | domain | label")
+
         t0 = time.monotonic()
-        queryset = Source.objects
-        if not options.get("no_index"):
-            queryset = queryset.filter(search_vector=q)
-        queryset = queryset.annotate(rank=r).filter(rank__gte=0.01).order_by("-rank")
-        if options.get("show_query"):
-            print(queryset.query)
-        for src in queryset[:10]:
-            print(f"{src.rank:.6f} | {src.id} | {src.name} | {src.label}")
+        src_resp = src_view(src_req)
         t1 = time.monotonic()
-        print(f"in {(t1 - t0):.6f} seconds", )
+        print(f"{(t1 - t0):.6f} seconds", )
+        if src_resp.status_code == 200:
+            d = src_resp.data
+            print("count", d["count"], "next", d["next"], "prev", d["previous"], "len", len(d["results"]))
+            for r in d["results"]:
+                print(r["id"], ">>", r["name"], ">>", r["label"], [a["domain"] for a in r["alternative_domains"]])
+        else:
+            print("response", src_resp.status_code)
         print("")
 
+        ################
+        # https://search.mediacloud.org/api/sources/collections/?limit=100&name=york+times+new
+        coll_view = CollectionViewSet.as_view(actions={'get': 'list'})
+        coll_req = factory.get(f'/sources/collections/?limit=100&name={tokens}')
+        force_authenticate(coll_req, user=user)
+
         print("Collections")
-        print("rank | id | name")
-        v = SearchVector("name", weight="A")
-        queryset = Collection.objects.annotate(rank=SearchRank(v, q)).filter(rank__gte=0.05).order_by("-rank")
-        for coll in queryset[:10]:
-            print(f"{coll.rank:.6f} | {coll.id} | {coll.name}")
-        t2 = time.monotonic()
-        print(f"in {(t2-t1):.6f} seconds", )
+        t0 = time.monotonic()
+        coll_resp = coll_view(coll_req)
+        t1 = time.monotonic()
+        print(f"{(t1 - t0):.6f} seconds", )
 
-
+        if coll_resp.status_code == 200:
+            d = coll_resp.data
+            print("count", d["count"], "next", d["next"], "prev", d["previous"], "len", len(d["results"]))
+            for r in d["results"]:
+                print(r["id"], ">>", r["name"], ">>", r["source_count"], "srcs")
+        else:
+            print("response", coll_resp.status_code)
+        print("")
