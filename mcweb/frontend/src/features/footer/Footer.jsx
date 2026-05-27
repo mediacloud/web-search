15e9398f0d244e876e3bfad98ac6f2cbc3b221c3
Phil Budne <phil@regressive.org>
2026-05-27 12:01:47 -0400

    Update footer citation URL

--- mcweb/frontend/src/features/footer/Footer.jsx
+++ mcweb/frontend/src/features/footer/Footer.jsx
@@ -62,7 +62,7 @@ function Footer() {
         <p>
           Using Media Cloud tools and data?
           {' '}
-          <a href="https://ojs.aaai.org/index.php/ICWSM/article/view/18127" target="_blank" rel="noreferrer">
+          <a href="https://doi.org/10.1609/icwsm.v20i1.42778" target="_blank" rel="noreferrer">
             Please cite us.
           </a>
         </p>
