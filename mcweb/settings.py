b4f9e8073007a151c7bbcda30ff8242fcc9d0adf
Phil Budne <phil@regressive.org>
2026-06-12 23:21:32 -0400

    Update for v3.1.6

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__file__)
 # validate with
 # "jq . < mcweb/frontend/src/features/about/release_history.json"
 # (JSON reader doesn't handle trailing commas in lists/dicts)
-VERSION = "3.1.5"
+VERSION = "3.1.6"
 
 class Groups:
     CONTRIBUTOR = "contributor"
