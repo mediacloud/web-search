edff6eb9aa0365cb9068619b46cea70207113527
Phil Budne <phil@regressive.org>
2026-07-09 16:48:23 -0400

    initial version of mc-deploy baed dokku-scripts/deploy.py

---
+++ dokku-scripts/deploy.py
@@ -0,0 +1,105 @@
+"""
+web-search deploy script using mc-deploy (in system-dev-ops repo.
+
+Needs to be installed in dev venv, but not in container!!
+
+meant to replace shell scripts: push.sh, instance.sh, config.sh,
+common.sh, dburl.sh, clone-db.sh plus vars.py
+"""
+
+import sys
+
+from mc_deploy.dokku import DokkuDBDeploy
+from mc_deploy.django import SettingsVersionMixin, DjangoMixin
+
+
+class WebSearchDeploy(SettingsVersionMixin, DjangoMixin, DokkuDBDeploy):
+    # Much better to increase WEB_CONCURRENCY setting (gunicorn workers)
+    # than number of web containers (parallel containers don't cooperate,
+    # or report stats properly)!
+    DOKKU_SCALE = {"web": 1, "supervisord": 1}
+    PUBLIC_NAME = "search"      # w/o PUBLIC_DOMAIN
+
+    # map of plugin name to service name suffix:
+    DOKKU_SERVICES = {
+        "postgres": "-db",
+        "redis": "-cache",
+        "storage": ""
+    }
+
+    INST_BASE = "mcweb"         # app base name
+    PROJECT_REPO = "web-search"
+
+    SETTINGS_FILE = "mcweb/settings.py" # for SettingsVersionMixin
+
+    def settings_get_new(self) -> None:
+        """
+        load project settings
+        """
+        super().settings_get_new()
+
+        self.settings_add("STATSD_HOST", self.STATSD_HOST)
+        # STATSD_PREFIX provided by base!
+
+        # mcmetadata uses pylangid3 which uses numpy which uses
+        # libopenblas for vector math, creating by default one worker
+        # thread per virtual CPU, which sit and spin looking for work.
+        # Use just one thread.  The same setting is used in
+        # story-indexer (which actually does language identification),
+        # with less CPU time used!
+        self.settings_add("OPENBLAS_NUM_THREADS", "1")
+
+        # from push.sh, config.sh:
+        if self.is_prod_staging():
+            files = ["web-search.prod.sh"]
+            if self.is_staging():
+                files.append("web-search.staging.sh") # overrides
+            self.settings_load_private_files(f"{self.PROJECT_REPO}-config",
+                                             files)
+        else:
+            # load template config file for external development
+            # (avoid multiple places with default dev settings):
+            self.settings_load_file("mcweb/.env.template")
+
+            # but remove static, external database & redis URLs
+            # (dokku supplies those):
+            self.settings.pop("DATABASE_URL", None)
+            self.settings.pop("REDIS_URL", None)
+
+            user_conf = f"vars.{self.user}"
+            if not os.path.exists(user_conf):
+                with open(user_conf, "w") as f:
+                    print("creating {user_conf} for overrides")
+                    f.write("# put config overrides in this file\n")
+                    f.write("ADMIN_EMAIL= # gets alerts, scrape errors\n")
+                    # set system alert banner:
+                    f.write(
+                        f"""SYSTEM_ALERT="🚧 {self.user}'s dev instance 🚧"\n"""
+                    )
+            self.settings_load_file(user_conf)
+
+        # for prod/staging could almost CERTAINLY
+        # keep this in the static config!
+        app = self.inst_name
+        allowed: list[str] = []
+        if self.is_prod():
+            allowed.append(
+                f"{app}.{self.dokku_host_short}.{self.PUBLIC_DOMAIN}"
+            )
+            allowed.append(
+                f"{self.PUBLIC_NAME}.{self.PUBLIC_DOMAIN}"
+            )
+        else:
+            allowed.append(f"{app}.{self.dokku_host_fqdn}")
+            if self.is_staging():
+                base = self.get_inst_base() # without "staging-"
+                allowed.append(
+                    f"staging-{base}.{self.PUBLIC_HOST}.{self.PUBLIC_DOMAIN}"
+                )
+        self.debug("allowed", allowed)
+        self.settings_add("ALLOWED_HOSTS", ",".join(allowed))
+        print(self.settings)
+        sys.exit(1)
+
+d = WebSearchDeploy()
+sys.exit(d.run())
