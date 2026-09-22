0196f1f50102367931f11514baab4c733fe33136
Rahul Bhargava <rahulbot@gmail.com>
2026-09-22 11:51:26 +0200

    Django 6.1 needs PG contrib declared

--- mcweb/settings.py
+++ mcweb/settings.py
@@ -211,6 +211,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "rest_framework",
     "rest_framework.authtoken",
     "constance",
